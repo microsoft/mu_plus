62b6836114b8ed5ae33ef3905d939ff8a079162a
Kun Qin <kuqin@microsoft.com>
2019-01-11 18:19:48 +0000

    Merged PR 645: Removed unused options from PcBdsPkg
    Removed unused AutoPower config from PcBdsPkg

--- PcBdsPkg/Include/Settings/BootMenuSettings.h
+++ PcBdsPkg/Include/Settings/BootMenuSettings.h
@@ -31,7 +31,6 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define DFCI_SETTING_ID__IPV6                       "Device.IPv6Pxe.Enable"
 #define DFCI_SETTING_ID__BOOT_ORDER_LOCK            "Device.BootOrderLock.Enable"
 #define DFCI_SETTING_ID__ENABLE_USB_BOOT            "Device.USBBoot.Enable"
-#define DFCI_SETTING_ID__AUTO_POWERON_AFTER_LOSS    "Device.AutoPowerOn.Enable"
 
 #define DFCI_SETTING_ID__ALT_BOOT                   "OEM.AlternateBoot.Enable"
 #define DFCI_SETTING_ID__START_NETWORK              "OEM.StartNetwork.Enable"
