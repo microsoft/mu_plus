8bf6e6fcc8a05a997f16945541e1f8378e41d90f
Bret Barkelew <brbarkel@microsoft.com>
2019-08-01 15:52:35 -0700

    Relicensing of MsGraphicsPkg is complete.

--- MsGraphicsPkg/DisplayEngineDxe/FormDisplay.c
+++ MsGraphicsPkg/DisplayEngineDxe/FormDisplay.c
@@ -12,14 +12,8 @@ from the MsThemeLib, there is no need for the CustomizedDisplayLib.
 
 Copyright (c) 2007 - 2018, Intel Corporation. All rights reserved.<BR>
 Copyright (c) 2014, Hewlett-Packard Development Company, L.P.
-Copyright (C) 2015 - 2018, Microsoft Corporation.
-This program and the accompanying materials
-are licensed and made available under the terms and conditions of the BSD License
-which accompanies this distribution.  The full text of the license may be found at
-http://opensource.org/licenses/bsd-license.php
-
-THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+Copyright (C) Microsoft Corporation. All rights reserved.
+SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
