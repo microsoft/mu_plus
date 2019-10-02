fdceec0bfbd9411acad8a856a52da712382c0068
Mike Turner <miketur@microsoft.com>
2019-10-02 00:08:37 +0000

    Merged PR 1584: Add workaround to DefaultDMask being left at zero when loading a V3 internal variable.
    The DefaultDMask is the permission mask for adding permissions.  A bug was introduced with the V3 version of the variable where the DefaultDMask was set to zero after a reboot.  This means the owner cannot set new permissions. This fix allows all users permission when the Variable is V3 and DefaultDMask == 0, and then changes the version to V4. Changing the version to V4 prevent this workaround from re-applying.
    
    (cherry picked from commit 879ee746e7e0733c0f71e9b2cee7ab22a6248f69)

--- DfciPkg/Library/DfciSettingPermissionLib/DfciSettingPermissionProvisioned.c
+++ DfciPkg/Library/DfciSettingPermissionLib/DfciSettingPermissionProvisioned.c
@@ -13,14 +13,15 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 
 //Define the local structure for the variable (this is for internal use only)
-// Variable namespace - Use gEfiCallerIdGuid since this is internal only
+// Variable name-space - Use gEfiCallerIdGuid since this is internal only
 
 #define VAR_NAME  L"_SPP"
 #define VAR_HEADER_SIG SIGNATURE_32('S', 'B', 'C', 'Z')
 #define VAR_VERSION_V1 (1)
 #define VAR_VERSION_V2 (2)
 #define VAR_VERSION_V3 (3)
-#define MAX_SIZE_FOR_VAR (1024 * 2)  
+#define VAR_VERSION_V4 (4)
+#define MAX_SIZE_FOR_VAR (1024 * 2)
 
 #pragma pack (push, 1)
 
@@ -32,12 +33,12 @@ typedef struct {
 typedef struct {
   DFCI_PACKET_SIGNATURE Header;         // 'S', 'B', 'C', 'Z'
                                      // Header Version = 1
-  UINT32 Version; 
-  UINT32 LowestSupportedVersion; 
+  UINT32 Version;
+  UINT32 LowestSupportedVersion;
   EFI_TIME CreatedOn;
   EFI_TIME SavedOn;
   DFCI_PERMISSION_MASK DefaultPMask; //For any ID not found in the entry list this is the permission
-  UINT16 NumberOfEntries;     // Number of entris in the PermTable 
+  UINT16 NumberOfEntries;     // Number of entries in the PermTable
   DFCI_PERM_INTERNAL_TABLE_ENTRY_V1 PermTable[];
 } DFCI_PERM_INTERNAL_PROVISONED_VAR_V1;
 
@@ -55,8 +56,8 @@ typedef struct {
   EFI_TIME CreatedOn;
   EFI_TIME SavedOn;
   DFCI_PERMISSION_MASK DefaultPMask; //For any ID not found in the entry list this is the permission
-  UINT16 NumberOfEntries;     // Number of entris in the PermTable
-  DFCI_PERM_INTERNAL_TABLE_ENTRY_V2 PermTableStart; // First variable lenght entry
+  UINT16 NumberOfEntries;     // Number of entries in the PermTable
+  DFCI_PERM_INTERNAL_TABLE_ENTRY_V2 PermTableStart; // First variable length entry
 } DFCI_PERM_INTERNAL_PROVISONED_VAR_V2;
 
 typedef struct {
@@ -68,35 +69,212 @@ typedef struct {
 
 typedef struct {
   DFCI_PACKET_SIGNATURE  Header;    // 'S', 'B', 'C', 'Z'
-                                    //  Version = 3
+                                    //  Version = 3 and Version = 4
   UINT32 Version;
   UINT32 LowestSupportedVersion;
   EFI_TIME CreatedOn;
   EFI_TIME SavedOn;
   DFCI_PERMISSION_MASK DefaultPMask; //For any ID not found in the entry list this is the permission
   DFCI_PERMISSION_MASK DefaultDMask; //For any ID not found in the entry list this is the delegated permission
-  UINT16 NumberOfEntries;     // Number of entris in the PermTable
-  DFCI_PERM_INTERNAL_TABLE_ENTRY PermTableStart; // First variable lenght entry
+  UINT16 NumberOfEntries;     // Number of entries in the PermTable
+  DFCI_PERM_INTERNAL_TABLE_ENTRY PermTableStart; // First variable length entry
 } DFCI_PERM_INTERNAL_PROVISONED_VAR;
 
 #pragma pack (pop)
 
+/**
+ * ConvertV1ToLatest
+ *
+ * Convert the ENUM setting ID's to String settings ID's
+ *
+ **/
+EFI_STATUS
+ConvertFromV1 (IN DFCI_PERMISSION_STORE            **Store,
+               IN DFCI_PERM_INTERNAL_PROVISONED_VAR *Var,
+               UINTN                                 VarSize) {
+
+  EFI_STATUS Status;
+  DFCI_PERM_INTERNAL_PROVISONED_VAR_V1 *Var1;
+  UINTN ComputedSize;
+  DFCI_SETTING_ID_STRING Id;
+
+
+  Var1 = (DFCI_PERM_INTERNAL_PROVISONED_VAR_V1 *) Var;
+  ComputedSize = sizeof(DFCI_PERM_INTERNAL_PROVISONED_VAR_V1) + (Var1->NumberOfEntries * sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY_V1));
+  if (VarSize != ComputedSize)
+  {
+    DEBUG((DEBUG_ERROR, "%a - VarSize (0x%X) != ComputedSize (0x%X)\n", __FUNCTION__, VarSize, ComputedSize));
+    ASSERT(VarSize == ComputedSize);
+    return EFI_COMPROMISED_DATA;
+  }
+  for (UINT16 i = 0; i < Var1->NumberOfEntries; i++)
+  {
+    Id = DfciV1TranslateEnum(Var1->PermTable[i].Id);
+    Status = AddPermissionEntry(*Store, Id, Var1->PermTable[i].Permissions, DFCI_IDENTITY_SIGNER_OWNER);
+    if (EFI_ERROR(Status))
+    {
+      DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry. %r\n", __FUNCTION__, Status));
+      ASSERT_EFI_ERROR(Status);
+      //continue anyway and hope for the best :)
+    }
+  }
+  if ((*Store)->DefaultPMask == DFCI_IDENTITY_LOCAL)
+  {
+    (*Store)->DefaultPMask = DFCI_PERMISSION_MASK__DEFAULT;
+  }
+  (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__DELEGATED_DEFAULT;
+
+  (*Store)->Modified = TRUE;
+  SaveToFlash(*Store);  // Complete the translation from V1 to latest version
+  DEBUG((DEBUG_INFO, "%a - Permission store converted to V%d.\n", __FUNCTION__, VAR_VERSION_V4));
+
+  return EFI_SUCCESS;
+}
+
+/**
+ * ConvertFromV2
+ *
+ * Initialize a DefaultDMASK, and update each of the Permission Entries with an default DMask
+ *
+ **/
+EFI_STATUS
+ConvertFromV2 (IN DFCI_PERMISSION_STORE            **Store,
+               IN DFCI_PERM_INTERNAL_PROVISONED_VAR *Var,
+               UINTN                                 VarSize) {
+
+  EFI_STATUS Status;
+  DFCI_PERM_INTERNAL_TABLE_ENTRY_V2    *PermEntry;
+  DFCI_PERM_INTERNAL_PROVISONED_VAR_V2 *Var2;
+  UINTN                                 Count;
+  CHAR8                                *PermPtr;
+  CHAR8                                *EndPtr;
+
+
+  Var2 = (DFCI_PERM_INTERNAL_PROVISONED_VAR_V2 *) Var;
+  PermPtr = (CHAR8 *) &Var2->PermTableStart;
+  EndPtr = (CHAR8 *) Var2;
+  EndPtr += VarSize;
+  Count = 0;
+  for (UINT16 i = 0; (i < Var2->NumberOfEntries) && (PermPtr < EndPtr); i++)
+  {
+    Count++;
+    PermEntry = (DFCI_PERM_INTERNAL_TABLE_ENTRY_V2 *) PermPtr;
+    PermPtr += (sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY_V2) + PermEntry->IdSize);
+
+    if (PermPtr > EndPtr)
+    {
+      DEBUG((DEBUG_ERROR, "%a - Poor calculation for variable size.\n", __FUNCTION__));
+      ASSERT(FALSE);
+      return EFI_COMPROMISED_DATA;
+    }
+    Status = AddPermissionEntry(*Store, (DFCI_SETTING_ID_STRING)PermEntry->Id, PermEntry->Permissions, DFCI_IDENTITY_SIGNER_OWNER);
+    if (EFI_ERROR(Status))
+    {
+      DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry for %a. %r\n", __FUNCTION__, PermEntry->Id, Status));
+      ASSERT_EFI_ERROR(Status);
+      //continue anyway and hope for the best :)
+    }
+  }
+  if (Count != Var2->NumberOfEntries)
+  {
+    DEBUG((DEBUG_ERROR, "%a - Failed to process all permission entries. %r\n", __FUNCTION__, Status));
+    Status = EFI_COMPROMISED_DATA;
+    ASSERT_EFI_ERROR(Status);
+    return Status;
+  }
+  if ((*Store)->DefaultPMask == DFCI_IDENTITY_LOCAL)
+  {
+    (*Store)->DefaultPMask |= DFCI_PERMISSION_MASK__DEFAULT ;
+  }
+  (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__DELEGATED_DEFAULT;
+  (*Store)->Modified = TRUE;
+  SaveToFlash(*Store);  // Complete the translation from V2 to latest version
+  DEBUG((DEBUG_INFO, "%a - Permission store converted to V%d.\n", __FUNCTION__, VAR_VERSION_V4));
+  return EFI_SUCCESS;
+}
+
+/**
+ * ConvertV3toLatest
+ *
+ * For a V3 store, DefaultDMask may be 0.  Correct this by allowing any DFCI user DMask permission.
+ **/
+EFI_STATUS
+ConvertFromV3 (IN DFCI_PERMISSION_STORE **Store) {
+
+  //  Due to a bug where DefaultDMask was not saved, allow all users to have DMask permission for
+  //  a version 3 var store.  When the var store changes to V4, this work around will no longer apply.
+  //  In the normal case, the next permission packet will set the proper DMask.
+  if ((*Store)->DefaultDMask == 0) {               // Don't allow a 0 to propagate into the permission store
+    (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__USERS;
+  }
+  (*Store)->Modified = TRUE;
+  SaveToFlash(*Store);                      //  Store the updated DefaultDMask, and update version to latest
+  DEBUG((DEBUG_INFO, "%a - Permission store converted to V%d.\n", __FUNCTION__, VAR_VERSION_V4));
+  return EFI_SUCCESS;
+}
+
+/**
+ * LoadCurrentFromFlash
+ *
+ * Load the current version store (V3 or V4) into memory
+ *
+ **/
+EFI_STATUS
+LoadCurrentFromFlash (IN DFCI_PERMISSION_STORE            **Store,
+                      IN DFCI_PERM_INTERNAL_PROVISONED_VAR *Var,
+                      IN UINTN                              VarSize) {
+
+  EFI_STATUS Status;
+  DFCI_PERM_INTERNAL_TABLE_ENTRY *PermEntry;
+  UINTN                           Count;
+  CHAR8                          *PermPtr;
+  CHAR8                          *EndPtr;
+
+  PermPtr = (CHAR8 *) &Var->PermTableStart;
+  EndPtr = (CHAR8 *) Var;
+  EndPtr += VarSize;
+  Count = 0;
+  for (UINT16 i = 0; (i < Var->NumberOfEntries) && (PermPtr < EndPtr); i++)
+  {
+    Count++;
+    PermEntry = (DFCI_PERM_INTERNAL_TABLE_ENTRY *) PermPtr;
+    PermPtr += (sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY) + PermEntry->IdSize);
+
+    if (PermPtr > EndPtr)
+    {
+      DEBUG((DEBUG_ERROR, "%a - Poor calculation for variable size.\n", __FUNCTION__));
+      ASSERT(FALSE);
+      return EFI_COMPROMISED_DATA;
+    }
+    Status = AddPermissionEntry(*Store, (DFCI_SETTING_ID_STRING)PermEntry->Id, PermEntry->Permissions, PermEntry->DMask);
+    if (EFI_ERROR(Status))
+    {
+      DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry for %a. %r\n", __FUNCTION__, PermEntry->Id, Status));
+      ASSERT_EFI_ERROR(Status);
+      //continue anyway and hope for the best :)
+    }
+  }
+  if (Count != Var->NumberOfEntries)
+  {
+    DEBUG((DEBUG_ERROR, "%a - Failed to process all permission entries. %r\n", __FUNCTION__, Status));
+    Status = EFI_COMPROMISED_DATA;
+    ASSERT_EFI_ERROR(Status);
+    return Status;;
+  }
+
+  (*Store)->DefaultDMask = Var->DefaultDMask;
+  return EFI_SUCCESS;
+}
 
 EFI_STATUS
 EFIAPI
 LoadFromFlash(IN DFCI_PERMISSION_STORE **Store)
 {
   EFI_STATUS Status;
-  DFCI_PERM_INTERNAL_PROVISONED_VAR_V1 *Var1 = NULL;
-  DFCI_PERM_INTERNAL_PROVISONED_VAR_V2 *Var2 = NULL;
   DFCI_PERM_INTERNAL_PROVISONED_VAR    *Var = NULL;
   UINTN VarSize = 0;
   UINTN ComputedSize = 0;
   UINT32 VarAttributes = 0;
-  DFCI_SETTING_ID_STRING Id;
-  UINTN                           Count;
-  CHAR8                          *PermPtr;
-  CHAR8                          *EndPtr;
 
   if (Store == NULL)
   {
@@ -113,7 +291,7 @@ LoadFromFlash(IN DFCI_PERMISSION_STORE **Store)
     if (Status == EFI_NOT_FOUND)
     {
       DEBUG((DEBUG_INFO, "%a - Var not found.  1st boot after flash?\n", __FUNCTION__));
-    } 
+    }
     else
     {
       DEBUG((DEBUG_ERROR, "%a - Error getting variable %r\n", __FUNCTION__, Status));
@@ -148,10 +326,6 @@ LoadFromFlash(IN DFCI_PERMISSION_STORE **Store)
     goto EXIT;
   }
 
-  // Two possibilities here.  V1 has old setting ENUMs, and we have to convert these to
-  // V2 setting strings here if we run into a V1 provisioned system. All fields except the
-  // PermTable are the same between V1 and V2.
-
   if (Var->Version < Var->LowestSupportedVersion)
   {
     DEBUG((DEBUG_ERROR, "%a - Version (0x%X) < LowestSupportedVersion (0x%X)\n", __FUNCTION__, Var->Version, Var->LowestSupportedVersion));
@@ -170,144 +344,64 @@ LoadFromFlash(IN DFCI_PERMISSION_STORE **Store)
     goto EXIT;
   }
 
+  //
+  // All of the versions of the Permission Store variable have the same layout from Header through
+  // DefaultPMask.
+  //
+
+  (*Store)->Modified = FALSE;  //since flash matches store modified is false
   (*Store)->Version = Var->Version;
   (*Store)->Lsv = Var->LowestSupportedVersion;
-  (*Store)->Modified = FALSE;  //since flash matches store modified is false
-  (*Store)->DefaultPMask = Var->DefaultPMask;
   (*Store)->CreatedOn = Var->CreatedOn;
   (*Store)->SavedOn = Var->SavedOn;
-  (*Store)->DefaultDMask = 0;
+  (*Store)->DefaultPMask = Var->DefaultPMask;
+  (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__DELEGATED_DEFAULT;
+
   InitializeListHead(&((*Store)->PermissionsListHead));
 
   switch (Var->Header.Version)
   {
+
+    //
+    // Original version, where settings are an ENUM.  Have to translate the setting id ENUM to a setting id string.
+    //
     case VAR_VERSION_V1:
-      {
-        Var1 = (DFCI_PERM_INTERNAL_PROVISONED_VAR_V1 *) Var;
-        ComputedSize = sizeof(DFCI_PERM_INTERNAL_PROVISONED_VAR_V1) + (Var1->NumberOfEntries * sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY_V1));
-        if (VarSize != ComputedSize)
-        {
-          DEBUG((DEBUG_ERROR, "%a - VarSize (0x%X) != ComputedSize (0x%X)\n", __FUNCTION__, VarSize, ComputedSize));
-          ASSERT(VarSize == ComputedSize);
-          Status = EFI_COMPROMISED_DATA;
-          goto EXIT;
-        }
-        for (UINT16 i = 0; i < Var1->NumberOfEntries; i++)
-        {
-          Id = DfciV1TranslateEnum(Var1->PermTable[i].Id);
-          Status = AddPermissionEntry(*Store, Id, Var1->PermTable[i].Permissions, DFCI_IDENTITY_SIGNER_OWNER);
-          if (EFI_ERROR(Status))
-          {
-            DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry. %r\n", __FUNCTION__, Status));
-            ASSERT_EFI_ERROR(Status);
-            //continue anyway and hope for the best :)
-          }
-        }
-        if ((*Store)->DefaultPMask == DFCI_IDENTITY_LOCAL)
-        {
-          (*Store)->DefaultPMask = DFCI_PERMISSION_MASK__DEFAULT;
-        }
-        (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__DELEGATED_DEFAULT;
-
-        SaveToFlash(*Store);  // Complete the translation from V1 to V2
-        DEBUG((DEBUG_INFO, "%a - Permission store converted to V2.\n", __FUNCTION__));
-      }
+      Status = ConvertFromV1 (Store, Var, VarSize);
       break;
 
+    //
+    // Second version.  Here we need to add the default DMask to the Permission store.
+    //
     case VAR_VERSION_V2:
-      {
-        DFCI_PERM_INTERNAL_TABLE_ENTRY_V2 *PermEntry;
+      Status = ConvertFromV2 (Store, Var, VarSize);
+      break;
 
-        Var2 = (DFCI_PERM_INTERNAL_PROVISONED_VAR_V2 *) Var;
-        PermPtr = (CHAR8 *) &Var2->PermTableStart;
-        EndPtr = (CHAR8 *) Var2;
-        EndPtr += VarSize;
-        Count = 0;
-        for (UINT16 i = 0; (i < Var2->NumberOfEntries) && (PermPtr < EndPtr); i++)
-        {
-          Count++;
-          PermEntry = (DFCI_PERM_INTERNAL_TABLE_ENTRY_V2 *) PermPtr;
-          PermPtr += (sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY_V2) + PermEntry->IdSize);
-
-          if (PermPtr > EndPtr)
-          {
-            DEBUG((DEBUG_ERROR, "%a - Poor calculation for variable size.\n", __FUNCTION__));
-            ASSERT(FALSE);
-            break;
-          }
-          Status = AddPermissionEntry(*Store, (DFCI_SETTING_ID_STRING)PermEntry->Id, PermEntry->Permissions, DFCI_IDENTITY_SIGNER_OWNER);
-          if (EFI_ERROR(Status))
-          {
-            DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry for %a. %r\n", __FUNCTION__, PermEntry->Id, Status));
-            ASSERT_EFI_ERROR(Status);
-            //continue anyway and hope for the best :)
-          }
-        }
-        if (Count != Var2->NumberOfEntries)
-        {
-          DEBUG((DEBUG_ERROR, "%a - Failed to process all permission entries. %r\n", __FUNCTION__, Status));
-          Status = EFI_COMPROMISED_DATA;
-          ASSERT_EFI_ERROR(Status);
-          goto EXIT;
-        }
-        if ((*Store)->DefaultPMask == DFCI_IDENTITY_LOCAL)
-        {
-          (*Store)->DefaultPMask |= DFCI_IDENTITY_SIGNER_ZTD ;
-        }
-        (*Store)->DefaultDMask = DFCI_PERMISSION_MASK__DELEGATED_DEFAULT;
-        SaveToFlash(*Store);  // Complete the translation from V2 to V3
-        DEBUG((DEBUG_INFO, "%a - Permission store converted to V3.\n", __FUNCTION__));
+    //
+    // Third version.  Here, the DefaultDMask may be zero and needs to be corrected.  The format is
+    // the same as the current version, but just needs to be fixed up.
+    //
+    case VAR_VERSION_V3:
+      Status = LoadCurrentFromFlash (Store, Var, VarSize);
+      if (!EFI_ERROR(Status)) {
+        Status = ConvertFromV3 (Store);
       }
       break;
 
-    case VAR_VERSION_V3:
-      {
-        DFCI_PERM_INTERNAL_TABLE_ENTRY *PermEntry;
-
-        (*Store)->DefaultDMask = Var->DefaultDMask;
-
-        PermPtr = (CHAR8 *) &Var->PermTableStart;
-        EndPtr = (CHAR8 *) Var;
-        EndPtr += VarSize;
-        Count = 0;
-        for (UINT16 i = 0; (i < Var->NumberOfEntries) && (PermPtr < EndPtr); i++)
-        {
-          Count++;
-          PermEntry = (DFCI_PERM_INTERNAL_TABLE_ENTRY *) PermPtr;
-          PermPtr += (sizeof(DFCI_PERM_INTERNAL_TABLE_ENTRY) + PermEntry->IdSize);
-
-          if (PermPtr > EndPtr)
-          {
-            DEBUG((DEBUG_ERROR, "%a - Poor calculation for variable size.\n", __FUNCTION__));
-            ASSERT(FALSE);
-            break;
-          }
-          Status = AddPermissionEntry(*Store, (DFCI_SETTING_ID_STRING)PermEntry->Id, PermEntry->Permissions, PermEntry->DMask);
-          if (EFI_ERROR(Status))
-          {
-            DEBUG((DEBUG_ERROR, "%a - Failed to add a permission entry for %a. %r\n", __FUNCTION__, PermEntry->Id, Status));
-            ASSERT_EFI_ERROR(Status);
-            //continue anyway and hope for the best :)
-          }
-        }
-        if (Count != Var->NumberOfEntries)
-        {
-          DEBUG((DEBUG_ERROR, "%a - Failed to process all permission entries. %r\n", __FUNCTION__, Status));
-          Status = EFI_COMPROMISED_DATA;
-          ASSERT_EFI_ERROR(Status);
-          goto EXIT;
-        }
-      }
+    // Current Version
+    case VAR_VERSION_V4:
+      Status = LoadCurrentFromFlash (Store, Var, VarSize);
       break;
 
     default:
       DEBUG((DEBUG_INFO, "%a - Var Header Version %d not supported.\n", __FUNCTION__, Var->Header.Version));
       Status = EFI_COMPROMISED_DATA;
-      goto EXIT;
       break;
   }
 
-  DEBUG((DEBUG_INFO, "%a - Loaded valid variable. Version %d.  Code=%r\n", __FUNCTION__, Var->Header.Version, Status));
+  if (!EFI_ERROR(Status)) {
+    DEBUG((DEBUG_INFO, "%a - Loaded valid variable. Version %d.  Code=%r\n", __FUNCTION__, VAR_VERSION_V4, Status));
+  }
+
 
 EXIT:
   if (Var != NULL)
@@ -322,6 +416,12 @@ EXIT:
   return Status;
 }
 
+/**
+ * SaveToFlash
+ *
+ *     Always stores the variable in the latest format.
+ *
+ **/
 EFI_STATUS
 EFIAPI
 SaveToFlash(IN DFCI_PERMISSION_STORE *Store)
@@ -365,7 +465,7 @@ SaveToFlash(IN DFCI_PERMISSION_STORE *Store)
     goto EXIT;
   }
   Var->Header.Hdr.Signature = VAR_HEADER_SIG;
-  Var->Header.Version = VAR_VERSION_V3;
+  Var->Header.Version = VAR_VERSION_V4;
   Var->Version = Store->Version;
   Var->LowestSupportedVersion = Store->Lsv;
   Var->DefaultPMask = Store->DefaultPMask;
@@ -395,7 +495,7 @@ SaveToFlash(IN DFCI_PERMISSION_STORE *Store)
     //Leave time zeroed by allocate zero pool
   }
   CopyMem(&(Var->SavedOn), &t, sizeof(Var->SavedOn));
-  
+
   Status = gRT->SetVariable(VAR_NAME, &gDfciInternalVariableGuid, DFCI_INTERNAL_VAR_ATTRIBUTES, VarSize, Var);
   if (EFI_ERROR(Status))
   {
