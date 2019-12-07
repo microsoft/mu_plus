7e7919580805570c866db65a0340d3e7a4e11393
Bret Barkelew <brbarkel@microsoft.com>
2019-12-07 07:11:25 +0000

    Merged PR 1779: Bring things up to speed with EDK2 CI style.
    Update for the style of EDK2 CI.

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -1,18 +1,20 @@
 # @file
 #
-# Copyright (c), Microsoft Corporation
+# Copyright (c) Microsoft Corporation.
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
 import os
+import logging
 from edk2toolext.environment import shell_environment
 from edk2toolext.invocables.edk2_ci_build import CiBuildSettingsManager
 from edk2toolext.invocables.edk2_ci_setup import CiSetupSettingsManager
-from edk2toolext.invocables.edk2_setup import SetupSettingsManager
+from edk2toolext.invocables.edk2_setup import SetupSettingsManager, RequiredSubmodule
 from edk2toolext.invocables.edk2_update import UpdateSettingsManager
+from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
 from edk2toollib.utility_functions import GetHostInfo
 
 
-class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsManager, SetupSettingsManager):
+class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManager, PrEvalSettingsManager):
 
     def __init__(self):
         self.ActualPackages = []
@@ -54,6 +56,7 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
         ''' return iterable of edk2 architectures supported by this build '''
         return ("IA32",
                 "X64",
+                "ARM",
                 "AARCH64")
 
     def GetTargetsSupported(self):
@@ -85,8 +88,8 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
 
         Raise Exception if a list_of_requested_architectures is not supported
         '''
-        requested_arch_set = set(list_of_requested_architectures)
-        unsupported = requested_arch_set - set(self.GetArchitecturesSupported())
+        unsupported = set(list_of_requested_architectures) - \
+            set(self.GetArchitecturesSupported())
         if(len(unsupported) > 0):
             logging.critical(
                 "Unsupported Architecture Requested: " + " ".join(unsupported))
@@ -115,17 +118,24 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
 
     def GetActiveScopes(self):
         ''' return tuple containing scopes that should be active for this process '''
-        scopes = ("corebuild", "project_mu", "cibuild")
+        scopes = ("cibuild","edk2-build")
 
         self.ActualToolChainTag = shell_environment.GetBuildVars().GetValue("TOOL_CHAIN_TAG", "")
 
-        if (GetHostInfo().os == "Linux" and "AARCH64" in self.ActualArchitectures and self.ActualToolChainTag.upper().startswith("GCC")):
-            scopes += ("gcc_aarch64_linux",)
-
-        if GetHostInfo().os == "Windows":
-            scopes += ("host-test-win",)
+        if GetHostInfo().os.upper() == "LINUX" and self.ActualToolChainTag.upper().startswith("GCC"):
+            if "AARCH64" in self.ActualArchitectures:
+                scopes += ("gcc_aarch64_linux",)
+            if "ARM" in self.ActualArchitectures:
+                scopes += ("gcc_arm_linux",)
 
         return scopes
+
+    def GetRequiredSubmodules(self):
+        ''' return iterable containing RequiredSubmodule objects.
+        If no RequiredSubmodules return an empty iterable
+        '''
+        rs=[]
+        return rs
 
     def GetName(self):
         return "MuPlus"
@@ -161,12 +171,6 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
             }
         ]
 
-    def GetRequiredSubmodules(self):
-        ''' return iterable containing RequiredSubmodule objects.
-        If no RequiredSubmodules return an empty iterable
-        '''
-        return []
-
 
     def GetPackagesPath(self):
         ''' Return a list of workspace relative paths that should be mapped as edk2 PackagesPath '''
@@ -177,4 +181,9 @@ class Settings(CiBuildSettingsManager, CiSetupSettingsManager, UpdateSettingsMan
 
     def GetWorkspaceRoot(self):
         ''' get WorkspacePath '''
-        return os.path.dirname(os.path.abspath(__file__))
+        return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    def FilterPackagesToTest(self, changedFilesList: list, potentialPackagesList: list) -> list:
+        ''' Filter potential packages to test based on changed files. '''
+        return []
+
