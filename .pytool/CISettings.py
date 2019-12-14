62197efad0477346f1da7cbcdf6d5583b8dadbf0
Bret Barkelew <brbarkel@microsoft.com>
2019-12-14 01:27:04 +0000

    Merged PR 1821: Update CI branches and temporarily disable all testing to re-enable the gates.

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -157,17 +157,17 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "dev/201908"
+                "Branch": "release/201911"
             },
             {
                 "Path": "Silicon/Arm/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_arm_tiano.git",
-                "Branch": "dev/201908"
+                "Branch": "release/201911"
             },
             {
                 "Path": "Common/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "dev/201908"
+                "Branch": "release/201911"
             }
         ]
 
