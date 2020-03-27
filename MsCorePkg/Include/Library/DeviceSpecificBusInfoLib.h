03f56f21047babd8b5c5fdff61d21e64a9a2cec2
Chris Ruffin <v-chruff@microsoft.com>
2020-03-27 18:44:42 +0000

    Merged PR 2263: DeviceSpecificBusInfoLib: simplify access to static table
    DeviceSpecificBusInfoLib: simplify access to static table
    
    Remove a level of indirection in the interface to make it easier to
    access a static array.  Extend the library to include minimum supported link speed.
    
    (cherry picked from commit 7f8b1bc0ace8d7554d7f12e9d149478e983df8b0)

--- MsCorePkg/Include/Library/DeviceSpecificBusInfoLib.h
+++ MsCorePkg/Include/Library/DeviceSpecificBusInfoLib.h
@@ -1,10 +1,11 @@
 /** @file 
 DeviceSpecificBusInfoLib.h
 
-An interface for creators to define PCI devices for which they want to check at boot.
-Simply create a DEVICE_PCI_INFO struct for every device you want read, and a not found error
-will be logged if it is not read. In cases where the device won't boot to the OS, this can
-quickly identify if the cause is a PCI device not being detected.
+An interface for platforms to define PCI devices which is checked
+at boot. Simply create an array of DEVICE_PCI_INFO structures for every
+device desired, and an error will be logged if it is not found on the bus. In
+cases where the device won't boot to the OS, this can help quickly identify
+if the cause is due to a PCI device not being detected.
 
 Copyright (c) Microsoft Corporation. All rights reserved.
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -18,7 +19,13 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
     UINTN       BusNumber
     UINTN       DeviceNumber
     UINTN       FunctionNumber
+    UINTN       MinimumGenSpeed
 **/
+
+#define PCIE_LINK_SPEED_GEN1 1
+#define PCIE_LINK_SPEED_GEN2 2
+#define PCIE_LINK_SPEED_GEN3 3
+
 typedef struct DEVICE_PCI_INFO
 { 
     CHAR8                 DeviceName[8]; //So it fits within the 64 bits of Additional Code 2 in section data
@@ -27,26 +34,20 @@ typedef struct DEVICE_PCI_INFO
     UINTN                 BusNumber;
     UINTN                 DeviceNumber;
     UINTN                 FunctionNumber;
+    UINTN                 MinimumGenSpeed;
 } DEVICE_PCI_INFO;
 
 /**
-  Returns the number of DEVICE_PCI_INFO pointers within allocated array
+  Returns a pointer to a static array of DEVICE_PCI_INFO structures and the length of the
+  array.
 
-  Within the function *DevicesArray SHOULD BE ALLOCATED via BootServices. The allocated array
-  WILL BE FREED BY THE CALLER The only data within the pool should be pointers to 
-  DEVICE_PCI_INFO structs
+  @param[in, out]   DevicesArray  Pointer to the head of an array of DEVICE_PCI_INFO structures.
+                                  The caller shall not free this array.
 
-  The DEVICE_PCI_INFO structs within the pool SHOULD BE STATIC because
-  they WILL NOT BE FREED by the caller if allocated
-
-  @param[in]        DevicesArray  Pointer to an array of DEVICE_PCI_INFO* which
-                                  should be allocated within this function and populated with
-                                  pointers to static DEVICE_PCI_INFO structs
-
-  @retval           UINTN         Number of DEVICE_PCI_INFO* within the allocated array
+  @retval           UINTN         Length of the returned array.
 
 **/
 UINTN
 GetPciCheckDevices(
-  IN OUT DEVICE_PCI_INFO ***DevicesArray
+  IN OUT DEVICE_PCI_INFO **DevicesArray
   );
