32283879e6849ada432b5c739db1d5332f0da569
Taylor Beebe <tabeebe@microsoft.com>
2021-06-17 18:40:58 +0000

    Update Set Memory Protection Setting Functionality in PEI
    Previously, trying to make a change to the memory protection settings while in PEI phase returned EFI_UNSUPPORTED. This PR allows the settings to be updated **if** the memory protection settings HOB entry has not yet been created. If successful, setting in PEI will produce the HOB entry with the desired setting and no further updates will be allowed in PEI. A consequence of this is that only one memory protection setting can be updated in PEI phase but as of now there is only one setting anyway.

--- MsCorePkg/Library/MemoryProtectionLib/PeiMemoryProtectionLib.c
+++ MsCorePkg/Library/MemoryProtectionLib/PeiMemoryProtectionLib.c
@@ -27,18 +27,19 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
   @param[out] Setting    UINT32 populated with desired bitmask for memory protection setting
 
 **/
+STATIC
 VOID
 GetMemoryProtectionDefaultSetting (
     IN    MEMORY_PROTECTION_VAR_TOKEN      VarToken, 
     OUT   UINT32                          *Setting
   )
 {
-  if(VarToken == MEM_PROT_GLOBAL_TOGGLE_SETTING) {
-    *Setting = (UINT32)(PcdGetBool(PcdDefaultMemoryProtectionGlobalToggle));
+  if (VarToken == MEM_PROT_GLOBAL_TOGGLE_SETTING) {
+    *Setting = (UINT32) (PcdGetBool (PcdDefaultMemoryProtectionGlobalToggle));
     return;
   }
 
-  ASSERT(FALSE);
+  ASSERT (FALSE);
 }
 
 /**
@@ -52,6 +53,7 @@ GetMemoryProtectionDefaultSetting (
   @retval EFI_DEVICE_ERROR      The variable could not be retrieved because of a device error
 
 **/
+STATIC
 EFI_STATUS
 PeiGetMemProtUefiVariable (
   OUT   MEM_PROT_SETTINGS  *Entry
@@ -59,85 +61,124 @@ PeiGetMemProtUefiVariable (
 {
   EFI_PEI_READ_ONLY_VARIABLE2_PPI *VariablePpi;
   EFI_STATUS                       Status;
-  UINTN                            Size = sizeof(MEM_PROT_SETTINGS);
+  UINTN                            Size = sizeof (MEM_PROT_SETTINGS);
 
   Status = PeiServicesLocatePpi (
               &gEfiPeiReadOnlyVariable2PpiGuid,
               0,
               NULL,
-              (VOID **)&VariablePpi
+              (VOID **) &VariablePpi
               );
 
-  if(!EFI_ERROR(Status)){
+  if (!EFI_ERROR (Status)){
 
-    Status = VariablePpi->GetVariable (VariablePpi,
-                                       MEMORY_PROTECTION_SETTINGS_VAR_NAME,
-                                       &gMemoryProtectionsGuid,
-                                       NULL,
-                                       &Size,
-                                       Entry);
+    Status = VariablePpi->GetVariable (
+                            VariablePpi,
+                            MEMORY_PROTECTION_SETTINGS_VAR_NAME,
+                            &gMemoryProtectionsGuid,
+                            NULL,
+                            &Size,
+                            Entry
+                            );
   }
 
   return Status;
 }
 
 /**
-  Creates the memory protections HOB entry by checking the Defaults, Overrides and UEFI variables
+  Uses input to build the memory protection guided HOB.
+
+  @param[in] Entry  Buffer containing a copy of the data written to the HOB
+  
+  @retval EFI_SUCCESS             HOB entry created
+  @retval EFI_INVALID_PARAMETER   Entry was NULL
+  @retval EFI_OUT_OF_RESOURCES    Failed to create HOB entry
+
+**/
+STATIC
+EFI_STATUS
+CreateMemoryProtectionHobEntry (
+  IN  MEM_PROT_SETTINGS  *Entry
+  )
+{
+  VOID *Ptr = NULL;
+
+  if (Entry == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  // Create the HOB entry
+  Ptr = BuildGuidDataHob (
+          &gHobMemoryProtectionsGuid,
+          Entry,
+          sizeof (MEM_PROT_SETTINGS)
+          );
+
+  if (Ptr != NULL) {
+    return EFI_SUCCESS;
+  }
+
+  return EFI_OUT_OF_RESOURCES;
+}
+
+/**
+  Generates the memory protections HOB entry by checking the Defaults, Overrides and UEFI variables
   on this platform.
 
   @param[out] Entry  Buffer containing a copy of the data written to the HOB
 
-  @retval EFI_SUCCESS             HOB entry created
-  @retval EFI_INVALID_PARAMETER   Failed to create HOB entry due to error in BuildGuidDataHob() call
-
 **/
-EFI_STATUS
+STATIC
+VOID
 GenerateMemoryProtectionHobEntry (
   OUT   MEM_PROT_SETTINGS  *Entry
   )
 {
-  MEM_PROT_SETTINGS  HobEntry;
   EFI_STATUS         Status;
-  UINT32             Setting   = 0;
-  VOID              *Ptr       = NULL;
+  UINT32             Setting    = 0;
 
-  Status = PeiGetMemProtUefiVariable(&HobEntry);
-
-  // Check if the memory protection variable exists and, if not, populate the setting with the PCD default
-  if(EFI_ERROR(Status)) {
-    GetMemoryProtectionDefaultSetting(MEM_PROT_GLOBAL_TOGGLE_SETTING, &Setting);
-    HobEntry.MemProtGlobalToggle = (BOOLEAN) Setting;
+  if (Entry == NULL) {
+    return;
   }
 
-  Status = MemoryProtectionExceptionOverrideCheck(MEM_PROT_GLOBAL_TOGGLE_SETTING, &Setting);
+  Status = PeiGetMemProtUefiVariable (Entry);
+
+  // Check if the memory protection variable exists and, if not, populate the setting with the PCD default
+  if (EFI_ERROR (Status)) {
+    GetMemoryProtectionDefaultSetting (
+      MEM_PROT_GLOBAL_TOGGLE_SETTING,
+      &Setting
+      );
+
+    Entry->MemProtGlobalToggle = (BOOLEAN) Setting;
+  }
+
+  Status = MemoryProtectionExceptionOverrideCheck (
+             MEM_PROT_GLOBAL_TOGGLE_SETTING,
+             &Setting
+             );
 
   // Check if an override exists in platform early store indicating that on a previous boot and exception
   // potentially related to memory protections was hit
-  if(!EFI_ERROR(Status)) {
-    HobEntry.MemProtGlobalToggle = (BOOLEAN) Setting;
+  if (!EFI_ERROR (Status)) {
+    Entry->MemProtGlobalToggle = (BOOLEAN) Setting;
   }
 
-  // Create the HOB entry
-  Ptr = BuildGuidDataHob(&gHobMemoryProtectionsGuid, &HobEntry, sizeof(MEM_PROT_SETTINGS));
-
-  if (Ptr != NULL) { 
-    CopyMem(Entry, Ptr, sizeof(MEM_PROT_SETTINGS));
-    return EFI_SUCCESS;
-  }
-
-  return EFI_INVALID_PARAMETER;
+  return;
 }
 
 /**
   Sets a memory protections setting. Valid MEMORY_PROTECTION_VAR_TOKEN values are
-  defined MemoryProtectionExceptionLib.h.
+  defined MemoryProtectionExceptionLib.h. NOTE: In PEI, a setting can only be set if
+  the HOB entry has not already been created. If successful, calling this function will
+  create the HOB entry.
 
   @param[in] VarToken   MEMORY_PROTECTION_VAR_TOKEN representing variable
   @param[in] Setting    UINT32 populated with desired bitmask for memory protection setting
 
-  @retval EFI_SUCCESS       Uefi variable has been updated
-  @retval EFI_NOT_READY     gBS hasn't been initialized yet
-  @retval EFI_NOT_FOUND     Could not find the variable
+  @retval EFI_SUCCESS             Uefi variable has been updated
+  @retval EFI_UNSUPPORTED         HOB Entry already created or failed to be created
+  @retval EFI_INVALID_PARAMETER   VarToken invalid
 
 **/
 EFI_STATUS
@@ -146,7 +187,28 @@ InternalSetMemoryProtectionSetting (
   IN    UINT32                           Setting
   )
 {
-  return EFI_UNSUPPORTED;
+  MEM_PROT_SETTINGS  Entry;
+  EFI_STATUS         Status;
+
+  Status = GetMemoryProtectionHobEntry (&Entry);
+
+  // HOB Entry already created - cannot update any setting now
+  if (!EFI_ERROR (Status)) {
+    return EFI_UNSUPPORTED;
+  }
+
+  GenerateMemoryProtectionHobEntry (&Entry);
+
+  switch (VarToken)
+  {
+    case MEM_PROT_GLOBAL_TOGGLE_SETTING:
+      Entry.MemProtGlobalToggle = (BOOLEAN) Setting;
+      break;
+    default:
+      return EFI_INVALID_PARAMETER;
+  }
+
+  return CreateMemoryProtectionHobEntry (&Entry);
 }
 
 /**
@@ -164,12 +226,22 @@ InternalGetMemoryProtectionSettings (
   OUT   MEM_PROT_SETTINGS  *Entry
   )
 {
-  EFI_STATUS Status = EFI_SUCCESS;
+  MEM_PROT_SETTINGS  HobEntry;
+  EFI_STATUS         Status = EFI_SUCCESS;
 
-  Status = GetMemoryProtectionHobEntry(Entry);
+  Status = GetMemoryProtectionHobEntry (Entry);
 
-  if(EFI_ERROR(Status)) {
-    Status = GenerateMemoryProtectionHobEntry(Entry);
+  if (EFI_ERROR (Status)) {
+    GenerateMemoryProtectionHobEntry (&HobEntry);
+    Status = CreateMemoryProtectionHobEntry (&HobEntry);
+
+    if (!EFI_ERROR (Status)) {
+      CopyMem (
+        Entry,
+        &HobEntry,
+        sizeof (MEM_PROT_SETTINGS)
+        );
+    }
   }
 
   return Status;
