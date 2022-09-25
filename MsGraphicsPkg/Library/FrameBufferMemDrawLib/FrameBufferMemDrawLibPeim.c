bcecfc056f5470dc5a52e1ed226029ef4402eb04
Mike Turner <miketur@microsoft.com>
2022-09-25 01:42:31 +0000

    Merged PR 6101: Correct PEI initialization in FrameBufferMemDrawLib
    ## Description
    
    The original code did not correctly get the PPI
    
    - [No ] Breaking change?
      - Will this change break pre-existing builds or functionality without action being taken?
    
    ## How This Was Tested
    
    Unable to properly test.
    
    ## Integration Instructions
    
    N/A

--- MsGraphicsPkg/Library/FrameBufferMemDrawLib/FrameBufferMemDrawLibPeim.c
+++ MsGraphicsPkg/Library/FrameBufferMemDrawLib/FrameBufferMemDrawLibPeim.c
@@ -15,6 +15,9 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/DebugLib.h>
 #include <Library/PeiServicesLib.h>
 
+STATIC EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE  mMode;
+STATIC BOOLEAN                            mInitialized = FALSE;
+
 /***
  Get Graphics Information
 */
@@ -24,15 +27,25 @@ GetGraphicsInfo (
   EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE  **Mode
   )
 {
-  EFI_STATUS  Status;
+  EFI_PEI_GRAPHICS_PPI  *GfxInitPpi = NULL;
+  EFI_STATUS            Status      = EFI_SUCCESS;
 
-  //
-  // Locate the PEI Graphics information.
-  //
-  Status = PeiServicesLocatePpi (&gEfiPeiGraphicsPpiGuid, 0, NULL, (VOID *)Mode);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "%a: Failed to locate Pei Graphics Information. Code=%r\n", __FUNCTION__, Status));
-    *Mode = NULL;
+  if (!mInitialized) {
+    Status = PeiServicesLocatePpi (&gEfiPeiGraphicsPpiGuid, 0, NULL, (VOID *)&GfxInitPpi);
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "%a: Failed to locate Pei Graphics Ppi. Code=%r\n", __FUNCTION__, Status));
+    } else {
+      Status = GfxInitPpi->GraphicsPpiGetMode (&mMode);
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "%a: GraphicsPpiGetMode failed %r.\n", __FUNCTION__, Status));
+      } else {
+        mInitialized = TRUE;
+      }
+    }
+  }
+
+  if (!EFI_ERROR (Status)) {
+    *Mode = &mMode;
   }
 
   return Status;
