microsoft/mu_plus

commit fd357a110984a67b85ae088c6803558115eb2d36

Merged PR 6103: Added support for fixed in RAM version of advanced logger MM access library
## Description

This change updated the logic to check the fixed memory buffer, when `PcdAdvancedLoggerFixedInRAM` is enabled, instead of always pulling from HOB, which may not exist in certain cases.

- [ ] Breaking change?
  - Will this change break pre-existing builds or functionality without action being taken?

## How This Was Tested

Tested on virtual platforms that supports `PcdAdvancedLoggerFixedInRAM` to verify the log is properly stored.

## Integration Instructions

N/A

AdvLoggerPkg/Library/AdvLoggerMmAccessLib/AdvLoggerMmAccessLib.c
@@ -96,21 +96,26 @@ AdvLoggerAccessInit (
   //
   // Locate the Logger Information block.
   //
-  GuidHob = GetFirstGuidHob (&gAdvancedLoggerHobGuid);
-  if (GuidHob == NULL) {
-    DEBUG ((DEBUG_ERROR, "%a: Advanced Logger Hob not found\n", __FUNCTION__));
+  if (FeaturePcdGet (PcdAdvancedLoggerFixedInRAM)) {
+    mLoggerInfo = (ADVANCED_LOGGER_INFO *)(VOID *)FixedPcdGet64 (PcdAdvancedLoggerBase);
   } else {
-    LogPtr      = (ADVANCED_LOGGER_PTR *)GET_GUID_HOB_DATA (GuidHob);
-    mLoggerInfo = ALI_FROM_PA (LogPtr->LogBuffer);
-    if (mLoggerInfo != NULL) {
-      mMaxAddress = mLoggerInfo->LogBuffer + mLoggerInfo->LogBufferSize;
+    GuidHob = GetFirstGuidHob (&gAdvancedLoggerHobGuid);
+    if (GuidHob == NULL) {
+      DEBUG ((DEBUG_ERROR, "%a: Advanced Logger Hob not found\n", __FUNCTION__));
+    } else {
+      LogPtr      = (ADVANCED_LOGGER_PTR *)GET_GUID_HOB_DATA (GuidHob);
+      mLoggerInfo = ALI_FROM_PA (LogPtr->LogBuffer);
     }
-
-    //
-    // If mLoggerInfo is NULL at this point, there is no Advanced Logger.
-    //
-    DEBUG ((DEBUG_INFO, "%a: LoggerInfo=%p\n", __FUNCTION__, mLoggerInfo));
   }
+
+  if (mLoggerInfo != NULL) {
+    mMaxAddress = mLoggerInfo->LogBuffer + mLoggerInfo->LogBufferSize;
+  }
+
+  //
+  // If mLoggerInfo is NULL at this point, there is no Advanced Logger.
+  //
+  DEBUG ((DEBUG_INFO, "%a: LoggerInfo=%p\n", __FUNCTION__, mLoggerInfo));
 
   //
   // PcdMaxVariableSize include the variable header and the variable name size.

AdvLoggerPkg/Library/AdvLoggerMmAccessLib/AdvLoggerMmAccessLib.inf
@@ -43,5 +43,9 @@
 
 [Pcd]
   gEfiMdeModulePkgTokenSpaceGuid.PcdMaxVariableSize
+  gAdvLoggerPkgTokenSpaceGuid.PcdAdvancedLoggerBase
+
+[FeaturePcd]
+  gAdvLoggerPkgTokenSpaceGuid.PcdAdvancedLoggerFixedInRAM
 
 [Depex]

commit 532c5b5c805a553effa7cb723720ab393eda1dfc

AdvLoggerPkg/AdvLoggerMmAccessLib: Fix cast conversion compiler error
Commit fd357a1 introduced a conversion cast that produces the
following VS(2019) build error:

```
AdvLoggerPkg\Library\AdvLoggerMmAccessLib\AdvLoggerMmAccessLib.c(100):
  'type cast': conversion from 'unsigned int' to 'void *' of greater size
```

This change casts the integer to UINTN before casting to a pointer.

Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

AdvLoggerPkg/Library/AdvLoggerMmAccessLib/AdvLoggerMmAccessLib.c
@@ -97,7 +97,7 @@ AdvLoggerAccessInit (
   // Locate the Logger Information block.
   //
   if (FeaturePcdGet (PcdAdvancedLoggerFixedInRAM)) {
-    mLoggerInfo = (ADVANCED_LOGGER_INFO *)(VOID *)FixedPcdGet64 (PcdAdvancedLoggerBase);
+    mLoggerInfo = (ADVANCED_LOGGER_INFO *)(VOID *)(UINTN)FixedPcdGet64 (PcdAdvancedLoggerBase);
   } else {
     GuidHob = GetFirstGuidHob (&gAdvancedLoggerHobGuid);
     if (GuidHob == NULL) {