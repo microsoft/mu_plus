2b00f2dc6920c0e0251420237e3f8637809c980c
Taylor Beebe <tabeebe@microsoft.com>
2023-04-04 12:28:37 -0700

    Add Support for the Parsing of the Access Flag Bit in the Paging Audit Windows Scripts
    Description
    
    Add support for the Access Flag to the paging audit Windows scripts. The
    Access Flag bit is analogous to the Valid bit on X64.
    
    - [x] Impacts functionality?
      - **Functionality** - Does the change ultimately impact how firmware functions?
      - Examples: Add a new library, publish a new PPI, update an algorithm, ...
    - [ ] Impacts security?
      - **Security** - Does the change have a direct security impact on an application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
      - **Breaking change** - Will anyone consuming this change experience a break
        in build or boot behavior?
      - Examples: Add a new library class, move a module to a different repo, call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
      - **Documentation** - Does the change contain explicit documentation additions
        outside direct code modifications (and comments)?
      - Examples: Update readme file, add feature readme file, link to documentation
        on an a separate Web page, ...
    
    How This Was Tested
    
    Running the test on SBSA
    
    Integration Instructions
    
    N/A

--- UefiTestingPkg/AuditTests/PagingAudit/Windows/BinaryParsing.py
+++ UefiTestingPkg/AuditTests/PagingAudit/Windows/BinaryParsing.py
@@ -65,17 +65,17 @@ def Parse4kPages(fileName, addressbits, architecture):
             if 0 == (ByteArray[byteZeroIndex + 0] + ByteArray[byteZeroIndex + 1] + ByteArray[byteZeroIndex + 2] + ByteArray[byteZeroIndex + 3] + ByteArray[byteZeroIndex + 4] + ByteArray[byteZeroIndex + 5] + ByteArray[byteZeroIndex + 6] + ByteArray[byteZeroIndex + 7]):
                 byteZeroIndex += 8
                 continue
-            Valid = ((ByteArray[byteZeroIndex + 0] & 0x1))
+            AccessFlag = ((ByteArray[byteZeroIndex + 1] & 0x4) >> 2)
             IsTable = ((ByteArray[byteZeroIndex + 0] & 0x2) >> 1)
             AccessPermisions = (((ByteArray[byteZeroIndex + 0] & 0xC0) >> 6))
             Sharability = ((ByteArray[byteZeroIndex + 1] & 0x3))
             Pxn         = ((ByteArray[byteZeroIndex + 6] & 0x20) >> 5)
             Uxn         = ((ByteArray[byteZeroIndex + 6] & 0x40) >> 6)
             PageTableBaseAddress = (int.from_bytes(ByteArray[byteZeroIndex: byteZeroIndex + 8], 'little')) & (0xFFFFFFFFF << 12)
-            logging.debug("4KB Page: 0x%s. Valid: %d. AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), Valid, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
+            logging.debug("4KB Page: 0x%s. Access Flag: %d. AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), AccessFlag, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
             byteZeroIndex += 8
             num += 1
-            pages.append(MemoryRange("TTEntry", "4k", Valid, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
+            pages.append(MemoryRange("TTEntry", "4k", AccessFlag, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
 
     logging.debug("%d entries found in file %s" % (num, fileName))
     return pages
@@ -108,17 +108,17 @@ def Parse2mPages(fileName, addressbits, architecture):
             if 0 == (ByteArray[byteZeroIndex + 0] + ByteArray[byteZeroIndex + 1] + ByteArray[byteZeroIndex + 2] + ByteArray[byteZeroIndex + 3] + ByteArray[byteZeroIndex + 4] + ByteArray[byteZeroIndex + 5] + ByteArray[byteZeroIndex + 6] + ByteArray[byteZeroIndex + 7]):
                 byteZeroIndex += 8
                 continue
-            Valid = ((ByteArray[byteZeroIndex + 0] & 0x1))
+            AccessFlag = ((ByteArray[byteZeroIndex + 1] & 0x4) >> 2)
             IsTable = ((ByteArray[byteZeroIndex + 0] & 0x2) >> 1)
             AccessPermisions = (((ByteArray[byteZeroIndex + 0] & 0xC0) >> 6))
             Sharability = ((ByteArray[byteZeroIndex + 1] & 0x3))
             Pxn         = ((ByteArray[byteZeroIndex + 6] & 0x20) >> 5)
             Uxn         = ((ByteArray[byteZeroIndex + 6] & 0x40) >> 6)
             PageTableBaseAddress = (int.from_bytes(ByteArray[byteZeroIndex: byteZeroIndex + 8], 'little')) & (0xFFFFFFFFF << 12)
-            logging.debug("2MB Page: 0x%s. Valid: %d. IsTable: %d AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), Valid, IsTable, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
+            logging.debug("2MB Page: 0x%s. Access Flag: %d. IsTable: %d AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), AccessFlag, IsTable, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
             byteZeroIndex += 8
             num += 1
-            pages.append(MemoryRange("TTEntry", "2m", Valid, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
+            pages.append(MemoryRange("TTEntry", "2m", AccessFlag, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
 
     logging.debug("%d entries found in file %s" % (num, fileName))
     return pages
@@ -151,17 +151,17 @@ def Parse1gPages(fileName, addressbits, architecture):
             if 0 == (ByteArray[byteZeroIndex + 0] + ByteArray[byteZeroIndex + 1] + ByteArray[byteZeroIndex + 2] + ByteArray[byteZeroIndex + 3] + ByteArray[byteZeroIndex + 4] + ByteArray[byteZeroIndex + 5] + ByteArray[byteZeroIndex + 6] + ByteArray[byteZeroIndex + 7]):
                 byteZeroIndex += 8
                 continue
-            Valid = ((ByteArray[byteZeroIndex + 0] & 0x1))
+            AccessFlag = ((ByteArray[byteZeroIndex + 1] & 0x4) >> 2)
             IsTable = ((ByteArray[byteZeroIndex + 0] & 0x2) >> 1)
             AccessPermisions = (((ByteArray[byteZeroIndex + 0] & 0xC0) >> 6))
             Sharability = ((ByteArray[byteZeroIndex + 1] & 0x3))
             Pxn         = ((ByteArray[byteZeroIndex + 6] & 0x20) >> 5)
             Uxn         = ((ByteArray[byteZeroIndex + 6] & 0x40) >> 6)
             PageTableBaseAddress = (int.from_bytes(ByteArray[byteZeroIndex: byteZeroIndex + 8], 'little')) & (0xFFFFFFFFF << 12)
-            logging.debug("1GB Page: 0x%s. Valid: %d. IsTable: %d AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), Valid, IsTable, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
+            logging.debug("1GB Page: 0x%s. Access Flag: %d. IsTable: %d AccessPermissions: %d. Sharability: %d. Pxn: %d. Uxn: %d. PageTableBaseAddress: %s" % (BytesToHexString(ByteArray[byteZeroIndex : byteZeroIndex + 8]), AccessFlag, IsTable, AccessPermisions, Sharability, Pxn, Uxn, hex(PageTableBaseAddress)))
             byteZeroIndex += 8
             num += 1
-            pages.append(MemoryRange("TTEntry", "1g", Valid, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
+            pages.append(MemoryRange("TTEntry", "1g", AccessFlag, (AccessPermisions & 0x2) >> 1, Sharability, Pxn, Uxn, PageTableBaseAddress, IsTable))
 
     logging.debug("%d entries found in file %s" % (num, fileName))
     return pages
