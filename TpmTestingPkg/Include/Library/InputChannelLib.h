f56ffd82140660743d6578ba62dfff8f896f7f48
Michael Kubacki <michael.kubacki@microsoft.com>
2023-11-06 08:09:41 -0500

    TpmTestingPkg: Add InputChannelLib
    Adds a new library class (InputChannelLib) that allows the TPM replay
    event log to be passed through a platform-specific mechanism.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- TpmTestingPkg/Include/Library/InputChannelLib.h
+++ TpmTestingPkg/Include/Library/InputChannelLib.h
@@ -1,19 +1,20 @@
 /** @file
-  TPM Replay Generic Input Channel Header
+  TPM Event Log Input Channel Library
+
+  Allows a TPM replay log to be passed through a custom interface.
 
   Copyright (c) Microsoft Corporation.
-
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-#ifndef TPM_REPLAY_INPUT_CHANNEL_H_
-#define TPM_REPLAY_INPUT_CHANNEL_H_
+#ifndef INPUT_CHANNEL_LIB_H
+#define INPUT_CHANNEL_LIB_H
 
 #include <Guid/TpmReplayEventLog.h>
 
 /**
-  Retrieves a TPM Replay Event Log from the highest priority input channel.
+  Retrieves a TPM Replay Event Log through a custom interface.
 
   @param[out] ReplayEventLog            A pointer to a pointer to the buffer to hold the event log data.
   @param[out] ReplayEventLogSize        The size of the data placed in the buffer.
@@ -22,13 +23,14 @@
   @retval    EFI_INVALID_PARAMETER  A pointer argument given is NULL.
   @retval    EFI_UNSUPPORTED        The function is not implemented yet. The arguments are not used.
   @retval    EFI_COMPROMISED_DATA   The event log data found is not valid.
-  @retval    EFI_NOT_FOUND          The event log data was not found in a FFS file.
+  @retval    EFI_NOT_FOUND          The event log data was not found. The input channel is ignored in this case.
 
 **/
 EFI_STATUS
-GetReplayEventLog (
-  OUT TPM_REPLAY_EVENT_LOG  **ReplayEventLog,
-  OUT UINTN                 *ReplayEventLogSize
+EFIAPI
+GetReplayEventLogFromCustomInterface (
+  OUT VOID   **ReplayEventLog,
+  OUT UINTN  *ReplayEventLogSize
   );
 
 #endif
