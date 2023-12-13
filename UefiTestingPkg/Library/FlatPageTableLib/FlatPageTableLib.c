c0279a458922ca473dde410d683118153e560147
Taylor Beebe <taylor.d.beebe@gmail.com>
2023-12-13 12:03:23 -0800

    FlatPageTableLib: Update GetRegionAccessAttributes()
    Description
    
    GetRegionAccessAttributes() takes an input region description and page
    table map and outputs the attributes of that region. Because it's
    common for multi-page regions to have varying attributes, the patch
    updates the function to return the attributes of the first
    attribute-contiguous range found in the region and report the actual
    size of that range. This allows the caller to collect the attributes
    of the region by calling the function repeatedly instead of needing
    to guess which subsection of the region has contiguous attributes.
    
    - [x] Impacts functionality?
      - **Functionality** - Does the change ultimately impact how firmware functions?
      - Examples: Add a new library, publish a new PPI, update an algorithm, ...
    - [ ] Impacts security?
      - **Security** - Does the change have a direct security impact on an application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
      - **Breaking change** - Will anyone consuming this change experience a break
        in build or boot behavior?
      - Examples: Add a new library class, move a module to a different repo, call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
      - **Documentation** - Does the change contain explicit documentation additions
        outside direct code modifications (and comments)?
      - Examples: Update readme file, add feature readme file, link to documentation
        on an a separate Web page, ...
    
    How This Was Tested
    
    Tested on Q35 by running the function.
    
    Integration Instructions
    
    N/A

--- UefiTestingPkg/Library/FlatPageTableLib/FlatPageTableLib.c
+++ UefiTestingPkg/Library/FlatPageTableLib/FlatPageTableLib.c
@@ -14,12 +14,14 @@
 #include <Library/BaseLib.h>
 #include <Library/SafeIntLib.h>
 #include <Library/FlatPageTableLib.h>
+#include <Library/DebugLib.h>
 
-// TRUE if A and B have overlapping intervals
+// TRUE if A and B have overlapping intervals.
+// The intervals are inclusive.
 #define CHECK_OVERLAP(AStart, AEnd, BStart, BEnd)   \
-  ((AEnd > AStart) && (BEnd > BStart) &&            \
-  ((AStart <= BStart && AEnd > BStart) ||           \
-  (BStart <= AStart && BEnd > AStart)))
+  ((AEnd >= AStart) && (BEnd >= BStart) &&          \
+  ((AStart <= BStart && AEnd >= BStart) ||          \
+  (BStart <= AStart && BEnd >= AStart)))
 
 /**
   Dumps the contents of the input PAGE_MAP to the debug log.
@@ -53,48 +55,66 @@ DumpPageMap (
 
 /**
   Checks the input flat page/translation table for the input region and converts the associated
-  table entries to EFI access attributes (EFI_MEMORY_XP, EFI_MEMORY_RO, EFI_MEMORY_RP). If the
-  access attributes vary across the region, EFI_NOT_FOUND is returned.
+  table entries to EFI access attributes (EFI_MEMORY_XP, EFI_MEMORY_RO, EFI_MEMORY_RP). The caller
+  of this function is responsible for checking ActualCheckedLength if the return value is
+  EFI_NOT_FOUND or EFI_NO_MAPPING. EFI_NOT_FOUND indicates that the attributes vary across
+  the region. EFI_NO_MAPPING indicates that the section from RegionStart to RegionStart +
+  ActualCheckedLength is not mapped. If ActualCheckedLength == RegionLength,
+  when EFI_NO_MAPPING is returned, the entire input region is not mapped.
 
-  @param[in]  Map                 Pointer to the PAGE_MAP struct to be parsed
-  @param[in]  Length              Length in bytes of the region
-  @param[in]  Length              Length of the region
-  @param[out] Attributes          EFI Attributes of the region
+  @param[in]  Map                     Pointer to the PAGE_MAP struct to be parsed
+  @param[in]  RegionStart             Starting address of the region to check.
+  @param[in]  RegionLength            Length, in bytes, of the region to check.
+  @param[out] Attributes              EFI Attributes of the region.
+  @param[out] ActualCheckedLength     The length checked from RegionStart.
+                                      If the region has varying attributes or the start
+                                      of the region is not mapped, this will be the
+                                      length from RegionStart to which the return
+                                      value applies.
 
-  @retval EFI_SUCCESS             The output Attributes is valid
-  @retval EFI_INVALID_PARAMETER   The flat translation table has not been built or
-                                  Attributes was NULL or Length was 0
-  @retval EFI_NOT_FOUND           The input region could not be found
-  @retval EFI_NO_MAPPING          The access attributes are not consistent across the region.
+  @retval EFI_SUCCESS             The region attributes were successfully determined.
+  @retval EFI_INVALID_PARAMETER   An input argument is invalid.
+  @retval EFI_ABORTED             The input PAGE_MAP is invalid.
+  @retval EFI_NOT_FOUND           The input region starting at RegionStart has varying
+                                  attributes. See ActualCheckedLength for the length of
+                                  the contiguous region with the same attributes as
+                                  the start of the input region.
+  @retval EFI_NO_MAPPING          The region starting at RegionStart is not mapped. If
+                                  ActualCheckedLength == RegionLength, the region is
+                                  not mapped at all. Otherwise, ActualCheckedLength will
+                                  be the length of the unmapped region from RegionStart.
 **/
 EFI_STATUS
 EFIAPI
 GetRegionAccessAttributes (
   IN PAGE_MAP  *Map,
-  IN UINT64    Address,
-  IN UINT64    Length,
-  OUT UINT64   *Attributes
+  IN UINT64    RegionStart,
+  IN UINT64    RegionLength,
+  OUT UINT64   *Attributes,
+  OUT UINT64   *ActualCheckedLength
   )
 {
   UINTN    Index;
   UINT64   EntryStartAddress;
   UINT64   EntryEndAddress;
-  UINT64   InputEndAddress;
+  UINT64   CurrentStartAddress;
+  UINT64   RegionEnd;
   BOOLEAN  FoundRange;
   UINT64   FoundAttributes;
   UINT64   FoundAttributesOriginal;
 
   if ((Map->Entries == NULL) || (Map->EntryCount == 0) ||
-      (Attributes == NULL) || (Length == 0))
+      (Attributes == NULL) || (ActualCheckedLength == NULL) || (RegionLength == 0))
   {
     return EFI_INVALID_PARAMETER;
   }
 
-  FoundRange      = FALSE;
-  Index           = 0;
-  InputEndAddress = 0;
+  FoundRange          = FALSE;
+  Index               = 0;
+  CurrentStartAddress = RegionStart;
+  RegionEnd           = 0;
 
-  if (EFI_ERROR (SafeUint64Add (Address, Length - 1, &InputEndAddress))) {
+  if (EFI_ERROR (SafeUint64Add (RegionStart, RegionLength - 1, &RegionEnd))) {
     return EFI_INVALID_PARAMETER;
   }
 
@@ -108,37 +128,58 @@ GetRegionAccessAttributes (
             )
           ))
     {
+      *ActualCheckedLength = 0;
       return EFI_ABORTED;
     }
 
-    if (CHECK_OVERLAP (Address, InputEndAddress, EntryStartAddress, EntryEndAddress)) {
-      if (!FoundRange) {
-        FoundAttributesOriginal  = 0;
-        FoundAttributesOriginal |= IsPageExecutable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_XP;
-        FoundAttributesOriginal |= IsPageWritable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RO;
-        FoundAttributesOriginal |= IsPageReadable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RP;
-        FoundRange               = TRUE;
-      } else {
-        FoundAttributes  = 0;
-        FoundAttributes |= IsPageExecutable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_XP;
-        FoundAttributes |= IsPageWritable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RO;
-        FoundAttributes |= IsPageReadable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RP;
-        if (FoundAttributesOriginal != FoundAttributes) {
+    if (CHECK_OVERLAP (CurrentStartAddress, RegionEnd, EntryStartAddress, EntryEndAddress)) {
+      FoundAttributes  = IsPageExecutable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_XP;
+      FoundAttributes |= IsPageWritable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RO;
+      FoundAttributes |= IsPageReadable (Map->Entries[Index].PageEntry) ? 0 : EFI_MEMORY_RP;
+
+      // There is a gap between the current address and the start of the entry.
+      if (EntryStartAddress > CurrentStartAddress) {
+        if (FoundRange) {
+          *Attributes          = FoundAttributesOriginal;
+          *ActualCheckedLength = CurrentStartAddress - RegionStart;
+          return EFI_NOT_FOUND;
+        } else {
+          *Attributes          = 0;
+          *ActualCheckedLength = EntryStartAddress - RegionStart;
           return EFI_NO_MAPPING;
         }
       }
 
-      Address = EntryEndAddress + 1;
+      if (!FoundRange) {
+        FoundAttributesOriginal = FoundAttributes;
+        FoundRange              = TRUE;
+      } else if (FoundAttributesOriginal != FoundAttributes) {
+        *Attributes          = FoundAttributesOriginal;
+        *ActualCheckedLength = CurrentStartAddress - RegionStart;
+        return EFI_NOT_FOUND;
+      }
+
+      // The entry end address is inclusive, so add one to get the next region start.
+      // If the addition overflows, the region is contiguous to the end of the address space.
+      if (EFI_ERROR (SafeUint64Add (EntryEndAddress, 1, &CurrentStartAddress))) {
+        *Attributes          = FoundAttributesOriginal;
+        *ActualCheckedLength = RegionLength;
+        return EFI_SUCCESS;
+      }
     }
 
-    if (EntryEndAddress >= InputEndAddress) {
+    if (CurrentStartAddress >= RegionEnd) {
       break;
     }
   } while (++Index < Map->EntryCount);
 
   if (FoundRange) {
-    *Attributes = FoundAttributesOriginal;
+    *Attributes          = FoundAttributesOriginal;
+    *ActualCheckedLength = CurrentStartAddress >= RegionEnd ? RegionLength : CurrentStartAddress - RegionStart;
+  } else {
+    *Attributes          = 0;
+    *ActualCheckedLength = RegionLength;
   }
 
-  return FoundRange ? EFI_SUCCESS : EFI_NOT_FOUND;
+  return FoundRange ? EFI_SUCCESS : EFI_NO_MAPPING;
 }
