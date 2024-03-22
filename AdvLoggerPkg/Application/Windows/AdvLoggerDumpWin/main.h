68e37e9ff1f777f0b1bcf5f21da29aa3c626f4b2
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2024-03-21 18:14:46 -0700

    Raw dump only

---
+++ AdvLoggerPkg/Application/Windows/AdvLoggerDumpWin/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#define EFI_ERROR 1
+#define CONS_ERROR 2
+#define FILE_ERROR 3
+#define LOG_ERROR 4
+#define SUCCESS 0
+#define MAX_VAR_LENGTH 1024*1024
