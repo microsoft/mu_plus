8e7f35e52cc958e77c3f5690529ddd596f35cd13
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2024-08-20 14:56:57 -0700

    AdvLoggerPkg: Remove MockAdvancedLogger.cpp
    Delete MockAdvancedLogger.cpp and move mock function definitions
    to MockAdvancedLogger.h.

--- AdvLoggerPkg/Test/Mock/Include/GoogleTest/Protocol/MockAdvancedLogger.h
+++ AdvLoggerPkg/Test/Mock/Include/GoogleTest/Protocol/MockAdvancedLogger.h
@@ -33,4 +33,23 @@ extern "C" {
   extern ADVANCED_LOGGER_PROTOCOL  *gALProtocol;
 }
 
+//
+// Mock function definitions in header file to prevent need of cpp file and
+// make it easier to consume in tests.
+// This is only suitable for protocol mocks.
+//
+
+MOCK_INTERFACE_DEFINITION (MockAdvancedLogger);
+MOCK_FUNCTION_DEFINITION (MockAdvancedLogger, gAL_AdvancedLoggerWriteProtocol, 4, EFIAPI);
+
+static ADVANCED_LOGGER_PROTOCOL  advancedLoggerInstance = {
+  ADVANCED_LOGGER_PROTOCOL_SIGNATURE,   // UINT32
+  ADVANCED_LOGGER_PROTOCOL_VERSION,     // UINT32
+  gAL_AdvancedLoggerWriteProtocol       // ADVANCED_LOGGER_WRITE_PROTOCOL
+};
+
+extern "C" {
+  ADVANCED_LOGGER_PROTOCOL  *gALProtocol = &advancedLoggerInstance;
+}
+
 #endif // MOCK_ADVANCED_LOGGER_LIB_H_
