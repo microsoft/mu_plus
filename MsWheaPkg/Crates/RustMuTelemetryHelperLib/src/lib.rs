7f0c29746e8d6e180c23c14fdb85d6c677bbb6b3
Christopher Zurcher <christopher.zurcher@microsoft.com>
2024-11-26 14:08:58 -0800

    Fix test builds

--- MsWheaPkg/Crates/RustMuTelemetryHelperLib/src/lib.rs
+++ MsWheaPkg/Crates/RustMuTelemetryHelperLib/src/lib.rs
@@ -36,13 +36,10 @@
 
 mod status_code_runtime;
 
+use boot_services::{BootServices, StandardBootServices};
 use mu_pi::protocols::status_code::{EfiStatusCodeType, EfiStatusCodeValue};
 use mu_pi::status_code::{EFI_ERROR_CODE, EFI_ERROR_MAJOR, EFI_ERROR_MINOR};
-use mu_rust_helpers::{
-    boot_services::{BootServices, StandardBootServices},
-    guid,
-    guid::guid,
-};
+use mu_rust_helpers::{guid, guid::guid};
 use r_efi::efi;
 use status_code_runtime::{ReportStatusCode, StatusCodeRuntimeProtocol};
 use uuid::uuid;
