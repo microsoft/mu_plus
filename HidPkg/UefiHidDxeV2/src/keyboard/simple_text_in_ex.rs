ef4b9411d6359ce4732f3fa56ed0a7983a5b44fe
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2025-06-01 14:28:15 -0700

    Build Hid rust module with 1.85 compiler
    This change moved the rust module to build with 1.85. As requirement
    tightens, the rust code and tests were updated to work with the new
    requirements.
    
    For details on how to complete these options and their meaning refer to
    [CONTRIBUTING.md](https://github.com/microsoft/mu/blob/HEAD/CONTRIBUTING.md).
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    - [x] Backport to release branch?
    
    This change was tested on physical platform.
    
    N/A
    
    ---------
    
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>
    (cherry picked from commit 27efac49fb3a76cdc32b65a6c24b0ec488ae52f5)

--- HidPkg/UefiHidDxeV2/src/keyboard/simple_text_in_ex.rs
+++ HidPkg/UefiHidDxeV2/src/keyboard/simple_text_in_ex.rs
@@ -11,7 +11,7 @@ use alloc::{boxed::Box, vec::Vec};
 use core::{ffi::c_void, ptr};
 
 use r_efi::{efi, protocols};
-use rust_advanced_logger_dxe::{debugln, DEBUG_ERROR};
+use rust_advanced_logger_dxe::{DEBUG_ERROR, debugln};
 
 use crate::{
     boot_services::UefiBootServices,
