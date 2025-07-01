c7c66e535af0fbbf59061bb49d8dd8d1c537580a
Kun Qin <kuqin@microsoft.com>
2025-07-01 13:08:12 -0700

    uncrustify

--- AdvLoggerPkg/UnitTests/LineParser/LineParserTestApp.c
+++ AdvLoggerPkg/UnitTests/LineParser/LineParserTestApp.c
@@ -27,8 +27,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define UNIT_TEST_APP_NAME     "LineParser Library test cases"
 #define UNIT_TEST_APP_VERSION  "1.0"
 
-#define ADV_LOG_TIME_STAMP_RESULT       "hh:mm:ss:ttt : "
-#define ADV_LOG_TIME_STAMP_RESULT_LEN   (sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1)
+#define ADV_LOG_TIME_STAMP_RESULT      "hh:mm:ss:ttt : "
+#define ADV_LOG_TIME_STAMP_RESULT_LEN  (sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1)
 
 // The following text represents the file logger stream of text as individual DEBUG
 // statements.
