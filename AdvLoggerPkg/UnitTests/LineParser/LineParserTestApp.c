8ef20992cef0b186e231d69bc12b96c43b4ea770
Kun Qin <kuqin@microsoft.com>
2025-07-01 08:50:39 -0700

    PR feedback

--- AdvLoggerPkg/UnitTests/LineParser/LineParserTestApp.c
+++ AdvLoggerPkg/UnitTests/LineParser/LineParserTestApp.c
@@ -27,7 +27,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define UNIT_TEST_APP_NAME     "LineParser Library test cases"
 #define UNIT_TEST_APP_VERSION  "1.0"
 
-#define ADV_LOG_TIME_STAMP_RESULT     "hh:mm:ss:ttt : "
+#define ADV_LOG_TIME_STAMP_RESULT       "hh:mm:ss:ttt : "
+#define ADV_LOG_TIME_STAMP_RESULT_SIZE  (sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1)
 
 // The following text represents the file logger stream of text as individual DEBUG
 // statements.
@@ -566,18 +567,18 @@ BasicTests (
   UT_LOG_INFO ("\nExpected Length=%d\n", AsciiStrLen (Btc->ExpectedLine));
   UT_LOG_INFO ("\n = %a =\n", Btc->ExpectedLine);
 
-  // if (mMessageEntry.MessageLen != AsciiStrLen (Btc->ExpectedLine)) {
+  if (mMessageEntry.MessageLen != AsciiStrLen (Btc->ExpectedLine)) {
     DUMP_HEX (DEBUG_ERROR, 0, mMessageEntry.Message, mMessageEntry.MessageLen, "Actual   - ");
     DUMP_HEX (DEBUG_ERROR, 0, Btc->ExpectedLine, AsciiStrLen (Btc->ExpectedLine), "Expected - ");
-  // }
+  }
 
   UT_ASSERT_EQUAL (mMessageEntry.MessageLen, AsciiStrLen (Btc->ExpectedLine));
 
   // The following also verifies that the string is NULL terminated.
   UT_ASSERT_MEM_EQUAL (
-    &(mMessageEntry.Message[sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1]),
-    &(Btc->ExpectedLine[sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1]),
-    mMessageEntry.MessageLen + sizeof (CHAR8) - (sizeof (ADV_LOG_TIME_STAMP_RESULT) - 1));
+    &(mMessageEntry.Message[ADV_LOG_TIME_STAMP_RESULT_SIZE]),
+    &(Btc->ExpectedLine[ADV_LOG_TIME_STAMP_RESULT_SIZE]),
+    mMessageEntry.MessageLen + sizeof (CHAR8) - ADV_LOG_TIME_STAMP_RESULT_SIZE);
 
   return UNIT_TEST_PASSED;
 }
