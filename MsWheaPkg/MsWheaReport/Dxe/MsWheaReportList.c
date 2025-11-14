9c5e187d49f6da41ac10abf5873f562d4a592493
Kun Qin <kuqin@microsoft.com>
2025-11-14 09:57:08 -0800

    uncrustify

--- MsWheaPkg/MsWheaReport/Dxe/MsWheaReportList.c
+++ MsWheaPkg/MsWheaReport/Dxe/MsWheaReportList.c
@@ -28,10 +28,10 @@ CreateNewEntry (
   IN MS_WHEA_ERROR_ENTRY_MD  *MsWheaEntryMD
   )
 {
-  MS_WHEA_LIST_ENTRY  *MsWheaListEntry = NULL;
-  MS_WHEA_ERROR_ENTRY_MD  *TempMsWheaEntryMD = NULL;
-  MS_WHEA_ERROR_EXTRA_SECTION_DATA *ExtraSectionPtr = NULL;
-  UINT32              Index            = 0;
+  MS_WHEA_LIST_ENTRY                *MsWheaListEntry   = NULL;
+  MS_WHEA_ERROR_ENTRY_MD            *TempMsWheaEntryMD = NULL;
+  MS_WHEA_ERROR_EXTRA_SECTION_DATA  *ExtraSectionPtr   = NULL;
+  UINT32                            Index              = 0;
 
   // Input argument sanity check
   if (MsWheaEntryMD == NULL) {
@@ -57,12 +57,12 @@ CreateNewEntry (
   CopyMem (&((UINT8 *)MsWheaListEntry->PayloadPtr)[Index], MsWheaEntryMD, sizeof (MS_WHEA_ERROR_ENTRY_MD));
 
   TempMsWheaEntryMD = (MS_WHEA_ERROR_ENTRY_MD *)MsWheaListEntry->PayloadPtr;
-  ExtraSectionPtr = (MS_WHEA_ERROR_EXTRA_SECTION_DATA *)(UINTN)(MsWheaEntryMD->ExtraSection);
+  ExtraSectionPtr   = (MS_WHEA_ERROR_EXTRA_SECTION_DATA *)(UINTN)(MsWheaEntryMD->ExtraSection);
   if (ExtraSectionPtr != NULL) {
     TempMsWheaEntryMD->ExtraSection = (EFI_PHYSICAL_ADDRESS)(UINTN)AllocateCopyPool (
-                                      sizeof (MS_WHEA_ERROR_EXTRA_SECTION_DATA) + ExtraSectionPtr->DataSize,
-                                      ExtraSectionPtr
-                                      );
+                                                                     sizeof (MS_WHEA_ERROR_EXTRA_SECTION_DATA) + ExtraSectionPtr->DataSize,
+                                                                     ExtraSectionPtr
+                                                                     );
   }
 
   Index                                                               += sizeof (MS_WHEA_ERROR_ENTRY_MD);
