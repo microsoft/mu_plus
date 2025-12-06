05554be4a2ee7cc35f401c46492e02746c323c9d
Eeshan Londhe <eeshanlondhe@microsoft.com>
2025-12-05 16:20:42 -0800

    Add allow list to DMAProtectionAudit tests for IVRS. (#802)
    ## Description
    
    Add allow list to DMAProtectionAudit tests for IVRS.
    
    Allow list to now include [EfiReservedMemoryType, EfiACPIMemoryNVS]
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    N/A
    
    ## Integration Instructions
    
    N/A

--- UefiTestingPkg/AuditTests/DMAProtectionAudit/UEFI/IVRS/DMAProtectionTestArch.c
+++ UefiTestingPkg/AuditTests/DMAProtectionAudit/UEFI/IVRS/DMAProtectionTestArch.c
@@ -26,6 +26,39 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 /// ================================================================================================
 /// ================================================================================================
 ///
+/// HELPER FUNCTIONS
+///
+/// ================================================================================================
+/// ================================================================================================
+
+/**
+ * Check if the given memory type is allowed.
+ *
+ * @param MemoryType The memory type to check.
+ * @return TRUE if the memory type is allowed, FALSE otherwise.
+ *         Returns TRUE if MemoryType is in the allowed list: [EfiReservedMemoryType, EfiACPIMemoryNVS]
+ */
+BOOLEAN
+IsMemoryTypeAllowed (
+  IN EFI_MEMORY_TYPE  MemoryType
+  )
+{
+  UINTN  Index;
+
+  EFI_MEMORY_TYPE  AllowedMemoryTypes[] = { EfiReservedMemoryType, EfiACPIMemoryNVS };
+
+  for (Index = 0; Index < ARRAY_SIZE (AllowedMemoryTypes); Index++) {
+    if (MemoryType == AllowedMemoryTypes[Index]) {
+      return TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+/// ================================================================================================
+/// ================================================================================================
+///
 /// TEST CASES
 ///
 /// ================================================================================================
@@ -106,9 +139,9 @@ CheckExcludedRegions (
     if (  (EfiMemNext->PhysicalStart <= Current->IVMD->IVMDStartAddress)
        && ((EfiMemNext->PhysicalStart + EFI_PAGE_SIZE * EfiMemNext->NumberOfPages) >= (Current->IVMD->IVMDStartAddress + Current->IVMD->IVMDMemoryBlockLength)))
     {
-      // Verify memory range is marked as reserved
-      UT_ASSERT_EQUAL (EfiMemNext->Type, EfiACPIMemoryNVS);
-      UT_LOG_INFO ("IVMDs between %X and %X found with type EfiACPIMemoryNVS\n", Current->IVMD->IVMDStartAddress, Current->IVMD->IVMDStartAddress + Current->IVMD->IVMDMemoryBlockLength);
+      // Verify memory range type is in the allow list
+      UT_ASSERT_TRUE (IsMemoryTypeAllowed (EfiMemNext->Type));
+      UT_LOG_INFO ("IVMDs between %X and %X found with type %d\n", Current->IVMD->IVMDStartAddress, Current->IVMD->IVMDStartAddress + Current->IVMD->IVMDMemoryBlockLength, EfiMemNext->Type);
 
       // Move on to next IVMD and start search at beginning of memory map again
       EfiMemNext = EfiMemoryMap;
--- UefiTestingPkg/AuditTests/DMAProtectionAudit/UEFI/README.md
+++ UefiTestingPkg/AuditTests/DMAProtectionAudit/UEFI/README.md
@@ -12,8 +12,9 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 Shell based UEFI unit test based off of the UnitTestFrameworkPkg that test for:
 
 1. IOMMU status register shows IOMMU enabled
-2. All excluded regions are set as EfiReservedMemoryType(VTd)/EfiACPIMemoryNVS(IVRS)
-3. Bus mastering enabled (BME) is disabled on ExitBootServices. Because we can no longer write to file after ExitBootServices
+2. All excluded regions are set as EfiReservedMemoryType for VTd.
+3. All excluded regions are set as (EfiReservedMemoryType or EfiACPIMemoryNVS) for IVRS.
+4. Bus mastering enabled (BME) is disabled on ExitBootServices. Because we can no longer write to file after ExitBootServices
     a variable is used to store the test state and the machine.
 
 Note: this unit test requires a restart to finish its testing. If you plan to use this unit test in automation make sure
