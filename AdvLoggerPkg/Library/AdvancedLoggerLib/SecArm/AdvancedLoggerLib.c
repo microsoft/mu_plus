ccdbaaae9dbcb617ddbb1bff56fb3a78333a3ae3
Kun Qin <kun.qin@microsoft.com>
2025-12-28 06:40:49 +0000

    SEC arm version...

---
+++ AdvLoggerPkg/Library/AdvancedLoggerLib/SecArm/AdvancedLoggerLib.c
@@ -0,0 +1,110 @@
+/** @file
+  SEC implementation of the Advanced Logger library.
+
+  Copyright (c) Microsoft Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Uefi.h>
+
+#include <AdvancedLoggerInternal.h>
+
+#include <Library/AdvancedLoggerLib.h>
+#include <Library/AdvancedLoggerHdwPortLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/PcdLib.h>
+#include <Library/DebugLib.h>
+#include <Library/SynchronizationLib.h>
+
+#include "../AdvancedLoggerCommon.h"
+
+EFI_STATUS
+EFIAPI
+AdvancedLoggerLibConstructor (
+  VOID
+  )
+{
+  ADVANCED_LOGGER_PTR  *LogPtr;
+  ADVANCED_LOGGER_INFO    *LoggerInfo;
+
+  // Initialize the fixed memory LogPtr structure to no address, with a signature.
+  UINTN LogBufferSize;
+  LogBufferSize = EFI_PAGES_TO_SIZE (FixedPcdGet64 (PcdAdvancedLoggerPages));
+  
+  LogPtr = (ADVANCED_LOGGER_PTR *)(UINTN)FixedPcdGet64 (PcdAdvancedLoggerBase);
+  if (LogPtr != NULL) {
+    LoggerInfo = ALI_FROM_PA (LogPtr);
+    ZeroMem ((VOID *)LoggerInfo, sizeof (ADVANCED_LOGGER_INFO));
+    LoggerInfo->Signature          = ADVANCED_LOGGER_SIGNATURE;
+    LoggerInfo->Version            = ADVANCED_LOGGER_VERSION;
+    LoggerInfo->LogBufferSize      = (UINT32)(LogBufferSize - sizeof (ADVANCED_LOGGER_INFO));
+    LoggerInfo->LogBufferOffset    = EXPECTED_LOG_BUFFER_OFFSET (LoggerInfo);
+    LoggerInfo->LogCurrentOffset   = LoggerInfo->LogBufferOffset;
+    LoggerInfo->HdwPortInitialized = TRUE;
+    LoggerInfo->HwPrintLevel       = FixedPcdGet32 (PcdAdvancedLoggerHdwPortDebugPrintErrorLevel);
+    LoggerInfo->InPermanentRAM     = TRUE;
+    AdvancedLoggerHdwPortInitialize ();
+    DEBUG ((DEBUG_INFO, "%a: Advanced Logger initialized at fixed RAM address %p\n", __FUNCTION__, LoggerInfo));
+    // Dump the header contents for debug
+    DEBUG ((DEBUG_INFO, "%a: Logger Info Header:\n", __FUNCTION__));
+    DEBUG ((DEBUG_INFO, "  Signature:          0x%08X\n", LoggerInfo->Signature));
+    DEBUG ((DEBUG_INFO, "  Version:            0x%04X\n", LoggerInfo->Version));
+    DEBUG ((DEBUG_INFO, "  LogBufferOffset:    0x%08X\n", LoggerInfo->LogBufferOffset));
+    DEBUG ((DEBUG_INFO, "  LogCurrentOffset:   0x%08X\n", LoggerInfo->LogCurrentOffset));
+    DEBUG ((DEBUG_INFO, "  LogBufferSize:      0x%08X\n", LoggerInfo->LogBufferSize));
+  }
+
+  return EFI_SUCCESS;
+}
+
+/**
+  Get the Logger Information block
+
+ **/
+ADVANCED_LOGGER_INFO *
+EFIAPI
+AdvancedLoggerGetLoggerInfo (
+  VOID
+  )
+{
+  ADVANCED_LOGGER_INFO  *LoggerInfoSec;
+  ADVANCED_LOGGER_PTR   *LogPtr;
+
+  // The SEC implementation requires a priori knowledge of an address in the heap to
+  // use for the Logger Info block.
+
+  // The PCD AdvancedLoggerBase MAY be a 64 bit address.  However, it is
+  // trimmed to be a pointer the size of the actual platform SEC pointer - and
+  // the Pcd is expected to be set properly for the platform.
+
+  LoggerInfoSec = NULL;
+  LogPtr        = (ADVANCED_LOGGER_PTR *)(UINTN)FixedPcdGet64 (PcdAdvancedLoggerBase);
+
+  if ((LogPtr != NULL) &&
+      (LogPtr->Signature == ADVANCED_LOGGER_PTR_SIGNATURE) &&
+      (LogPtr->LogBuffer != 0ULL))
+  {
+    LoggerInfoSec = ALI_FROM_PA (LogPtr->LogBuffer);
+  }
+
+  return LoggerInfoSec;
+}
+
+/**
+  Helper function to return the log phase for each message.
+
+  This function is intended to be used to distinguish between
+  various types of modules.
+
+  @return       Phase of current advanced logger instance.
+**/
+UINT16
+EFIAPI
+AdvancedLoggerGetPhase (
+  VOID
+  )
+{
+  return ADVANCED_LOGGER_PHASE_SEC;
+}
