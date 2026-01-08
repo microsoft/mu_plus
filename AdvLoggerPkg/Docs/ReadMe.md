fc05e0be862d693c59efacbc8ec1a0125ab24cc7
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2026-01-07 21:50:58 -0800

    Adding advanced logger support for ARM's SEC phase (#810)
    ## Description
    
    The current setup does not have the support of advanced logger if an
    ARM64 platform would like to start the logging into static buffer from
    SEC phase.
    
    This change adds the support of this use case. The assumption is that
    the ARM64 platform will have a static carve out of the advanced log
    buffer and **hands off to DXE phase directly**, because PEI phase memory
    manager is not sophisticated enough to reserve carveout memory backed by
    DRAM and thus could run into memory corruption issues.
    
    For details on how to complete these options and their meaning refer to
    [CONTRIBUTING.md](https://github.com/microsoft/mu/blob/HEAD/CONTRIBUTING.md).
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    This is tested on QEMU SBSA PEI-less builds and booted to both UEFI
    shell and OS.
    
    ## Integration Instructions
    
    Add
    `AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/SecArm/AdvancedLoggerLib.inf`
    to the library SEC phase.
    
    ---------
    
    Co-authored-by: Oliver Smith-Denny <osde@microsoft.com>

--- AdvLoggerPkg/Docs/ReadMe.md
+++ AdvLoggerPkg/Docs/ReadMe.md
@@ -146,9 +146,12 @@ PEI components in the ARM with Security Processor configuration.
   AdvancedLoggerHdwPortLib|AdvLoggerPkg/Library/AdvancedLoggerHdwPortLib/AdvancedLoggerHdwPortLib.inf
   AdvancedLoggerAccessLib|AdvLoggerPkg/Library/AdvancedLoggerAccessLib/AdvancedLoggerAccessLib.inf
 
+!if $(PEILESS_SEC) == TRUE
+[LibraryClasses.common.SEC]
+  AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/PeilessArm/AdvancedLoggerLib.inf
+!else
 [LibraryClasses.common.SEC]
   AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/BaseArm/AdvancedLoggerLib.inf
-  AssertLib|AdvLoggerPkg/Library/AssertLib/AssertLib.inf
 
 [LibraryClasses.common.PEI_CORE]
   AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/PeiCore/AdvancedLoggerLib.inf
@@ -156,6 +159,7 @@ PEI components in the ARM with Security Processor configuration.
 [LibraryClasses.common.PEIM]
   AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/Pei/AdvancedLoggerLib.inf
   DebugLib|AdvLoggerPkg/Library/PeiDebugLibAdvancedLogger/PeiDebugLibAdvancedLogger.inf
+!endif
 
 [LibraryClasses.common.DXE_DRIVER, LibraryClasses.common.UEFI_DRIVER, LibraryClasses.common.UEFI_APPLICATION]
   AdvancedLoggerLib|AdvLoggerPkg/Library/AdvancedLoggerLib/Dxe/AdvancedLoggerLib.inf
---
+++ AdvLoggerPkg/Library/AdvancedLoggerLib/PeilessArm/AdvancedLoggerLib.c
@@ -0,0 +1,113 @@
+/** @file
+  SEC implementation of the Advanced Logger library for Aarch64 platforms.
+
+  Copyright (c) Microsoft Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi/UefiBaseType.h>
+
+#include <AdvancedLoggerInternal.h>
+
+#include <Library/AdvancedLoggerLib.h>
+#include <Library/AdvancedLoggerHdwPortLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/PcdLib.h>
+#include <Library/DebugLib.h>
+#include <Library/HobLib.h>
+#include <Library/SynchronizationLib.h>
+
+#include "../AdvancedLoggerCommon.h"
+
+EFI_STATUS
+EFIAPI
+AdvancedLoggerLibConstructor (
+  VOID
+  )
+{
+  ADVANCED_LOGGER_PTR   *LogPtr;
+  ADVANCED_LOGGER_INFO  *LoggerInfo;
+
+  // Initialize the fixed memory LogPtr structure to no address, with a signature.
+  UINTN  LogBufferSize;
+
+  LogBufferSize = EFI_PAGES_TO_SIZE (FixedPcdGet64 (PcdAdvancedLoggerPages));
+
+  LoggerInfo = ALI_FROM_PA (FixedPcdGet64 (PcdAdvancedLoggerBase));
+  if (LoggerInfo != NULL) {
+    ZeroMem ((VOID *)LoggerInfo, sizeof (ADVANCED_LOGGER_INFO));
+    LoggerInfo->Signature          = ADVANCED_LOGGER_SIGNATURE;
+    LoggerInfo->Version            = ADVANCED_LOGGER_VERSION;
+    LoggerInfo->LogBufferSize      = (UINT32)(LogBufferSize - sizeof (ADVANCED_LOGGER_INFO));
+    LoggerInfo->LogBufferOffset    = EXPECTED_LOG_BUFFER_OFFSET (LoggerInfo);
+    LoggerInfo->LogCurrentOffset   = LoggerInfo->LogBufferOffset;
+    LoggerInfo->HdwPortInitialized = TRUE;
+    LoggerInfo->HwPrintLevel       = FixedPcdGet32 (PcdAdvancedLoggerHdwPortDebugPrintErrorLevel);
+    LoggerInfo->InPermanentRAM     = TRUE;
+    AdvancedLoggerHdwPortInitialize ();
+    DEBUG ((DEBUG_INFO, "%a: Advanced Logger initialized at fixed RAM address %p\n", __func__, LoggerInfo));
+
+    // Dump the header contents for debug
+    DEBUG ((DEBUG_INFO, "%a: Logger Info Header:\n", __FUNCTION__));
+    DEBUG ((DEBUG_INFO, "  Signature:          0x%08X\n", LoggerInfo->Signature));
+    DEBUG ((DEBUG_INFO, "  Version:            0x%04X\n", LoggerInfo->Version));
+    DEBUG ((DEBUG_INFO, "  LogBufferOffset:    0x%08X\n", LoggerInfo->LogBufferOffset));
+    DEBUG ((DEBUG_INFO, "  LogCurrentOffset:   0x%08X\n", LoggerInfo->LogCurrentOffset));
+    DEBUG ((DEBUG_INFO, "  LogBufferSize:      0x%08X\n", LoggerInfo->LogBufferSize));
+
+    // Create the hob here so that DXE core can find it.
+    LogPtr            = BuildGuidHob (&gAdvancedLoggerHobGuid, sizeof (ADVANCED_LOGGER_INFO));
+    LogPtr->Signature = ADVANCED_LOGGER_PTR_SIGNATURE;
+    LogPtr->LogBuffer = PA_FROM_PTR (LoggerInfo);
+  }
+
+  return EFI_SUCCESS;
+}
+
+/**
+  Get the Logger Information block
+
+ **/
+ADVANCED_LOGGER_INFO *
+EFIAPI
+AdvancedLoggerGetLoggerInfo (
+  VOID
+  )
+{
+  ADVANCED_LOGGER_INFO  *LoggerInfoSec;
+  ADVANCED_LOGGER_PTR   *LogPtr;
+
+  // The SEC implementation requires a priori knowledge of an address in the heap to
+  // use for the Logger Info block.
+
+  LoggerInfoSec = NULL;
+  LogPtr        = (ADVANCED_LOGGER_PTR *)(UINTN)FixedPcdGet64 (PcdAdvancedLoggerBase);
+
+  if ((LogPtr != NULL) &&
+      (LogPtr->Signature == ADVANCED_LOGGER_PTR_SIGNATURE) &&
+      (LogPtr->LogBuffer != 0ULL))
+  {
+    LoggerInfoSec = ALI_FROM_PA (LogPtr->LogBuffer);
+  }
+
+  return LoggerInfoSec;
+}
+
+/**
+  Helper function to return the log phase for each message.
+
+  This function is intended to be used to distinguish between
+  various types of modules.
+
+  @return       Phase of current advanced logger instance.
+**/
+UINT16
+EFIAPI
+AdvancedLoggerGetPhase (
+  VOID
+  )
+{
+  return ADVANCED_LOGGER_PHASE_SEC;
+}
