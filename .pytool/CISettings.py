9835c3c676abe59f2aaf445c2337adbb54e7a067
Aaron Pop <aaronpop@microsoft.com>
2026-01-22 16:29:24 -0800

    Update CI for 202511

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -194,22 +194,13 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "release/202502"
-            },
-            {
-                "Path": "Silicon/Arm/MU_TIANO",
-                "Url": "https://github.com/Microsoft/mu_silicon_arm_tiano.git",
-                "Branch": "release/202502"
+                "Branch": "release/202511"
+                ""
             },
             {
                 "Path": "Silicon/Intel/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_intel_tiano.git",
-                "Branch": "release/202502"
-            },
-            {
-                "Path": "Common/MU_TIANO",
-                "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "release/202502"
+                "Branch": "release/202511"
             }
         ]
 
