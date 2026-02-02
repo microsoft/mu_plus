9973d3ef962677b66c8992d5c9b8d914fd7fbc03
Oliver Smith-Denny <osde@microsoft.com>
2026-02-02 13:09:45 -0800

    UefiTestingPkg: PagingAudit: Fix GetMemoryMap() Invocation
    The Paging Audit has a bad call pattern for GetMemoryMap():
    it assumes that the unmerged memory map size is within 20%
    larger than the merged memory map size (i.e. the first call
    to GetMemoryMap() with size 0 and the subsequent call).
    
    Instead, always use the unmerged size + 20% as the safe size
    to use. Otherwise we can fail because we say we have too small
    a buffer.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- UefiTestingPkg/AuditTests/PagingAudit/UEFI/PagingAuditCommon.c
+++ UefiTestingPkg/AuditTests/PagingAudit/UEFI/PagingAuditCommon.c
@@ -763,6 +763,10 @@ AllocateMemoryMapBuffer (
       goto FailureFreeMem;
     }
 
+    // ensure we store the unmerged buffer size as that is the required size.
+    // Add some padding.
+    mMemoryMapBufferSize = mMemoryMapSize + (mMemoryMapSize / 5);
+
     Status = gBS->GetMemoryMap (
                     &mMemoryMapSize,
                     mMemoryMap,
@@ -797,11 +801,6 @@ AllocateMemoryMapBuffer (
     goto FailureFreeMem;
   }
 
-  // mMemoryMapSize now contains the size of the filled in memory map. Increase
-  // it by 20% to account for any additional entries that may be required after
-  // other buffers are allocated.
-  mMemoryMapSize      += (mMemoryMapSize / 5);
-  mMemoryMapBufferSize = mMemoryMapSize;
   FreePool (mMemoryMap);
   mMemoryMap = AllocateZeroPool (mMemoryMapBufferSize);
 
