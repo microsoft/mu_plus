2def60870f39ecff9fdbf0c3987b01baf8ab5eca
Oliver Smith-Denny <osde@microsoft.com>
2026-04-01 14:53:45 -0700

    MsGraphicsPkg: Consume FltUsedLib from MsCorePkg
    Consume the new FltUsedLib in MsCorePkg instead of
    MdePkg.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MsCorePkg/Library/FltUsedLib/FltUsedLib.c
+++ MsCorePkg/Library/FltUsedLib/FltUsedLib.c
@@ -1,11 +1,13 @@
 /** @file
-  Lib to include if using floats
+  This library provides the _fltused symbol which is required to by MSVC and clang
+  when floating point operations are used. This library should be linked in for
+  any module that uses floating point operations.
 
-Copyright (C) Microsoft Corporation.
-SPDX-License-Identifier: BSD-2-Clause-Patent
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-// create a global to satisfy the compilers insertion of the _fltused
+// create a global to satisfy the compilers insertion of the _fltused symbol
 // in response to detecting floating point type operations.
 int  _fltused = 0x9875;
