3a492efbd4a6b2bdf317cb4ee614a43facce45de
Armando Ferreira <armando.ferreira@edu.uag.mx>
2026-05-12 08:58:02 -0700

    Address PR Comments

--- MsGraphicsPkg/SimpleWindowManagerDxe/WindowManager.c
+++ MsGraphicsPkg/SimpleWindowManagerDxe/WindowManager.c
@@ -1518,9 +1518,6 @@ GopRegisteredCallback (
     goto Exit;
   }
 
-  mAbsPointerMode.AbsoluteMaxX = mGop->Mode->Info->HorizontalResolution;
-  mAbsPointerMode.AbsoluteMaxY = mGop->Mode->Info->VerticalResolution;
-
   // Determine if the Simple Rendering Engine Protocol is available on the same Console Out handle.  The
   // Rendering Engine driver provides both Graphics Output and Rendering Engine protocols.
   //
@@ -1534,6 +1531,9 @@ GopRegisteredCallback (
     mGop = NULL;
     goto Exit;
   }
+
+  mAbsPointerMode.AbsoluteMaxX = mGop->Mode->Info->HorizontalResolution;
+  mAbsPointerMode.AbsoluteMaxY = mGop->Mode->Info->VerticalResolution;
 
   // Now that we found the Graphics Output Protocol, complete the second half of driver initialization.
   //
